9ffefde9b14f163dd6cbea4c9fdce5ff4ccb0669
rlk <>
1999-11-07 22:18:23 +0000

    Support Stylus Photo

--- print.c
+++ print.c
@@ -371,6 +371,8 @@ printer_t	printers[] =		/* List of supported printer types */
     escp2_parameters,	default_media_size,	escp2_imageable_area,	escp2_print },
   { N_("EPSON Stylus Photo EX"),	"escp2-ex",	1,	7,	0.585,	0.646,
     escp2_parameters,	default_media_size,	escp2_imageable_area,	escp2_print },
+  { N_("EPSON Stylus Photo EX"),	"escp2-photo",	1,	8,	0.585,	0.646,
+    escp2_parameters,	default_media_size,	escp2_imageable_area,	escp2_print },
 };
 
 
