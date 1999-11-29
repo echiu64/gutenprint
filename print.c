8b7e18ee92b1e916227297d2364c5900ba356a60
rlk <>
1999-11-29 12:17:37 +0000

    stdplugins-intl.h is not a standard header file

--- print.c
+++ print.c
@@ -61,7 +61,28 @@
 #endif
 
 #include <libgimp/gimpui.h>
+#if 0
 #include <libgimp/stdplugins-intl.h>
+#else
+#ifdef HAVE_LC_MESSAGES
+#define INIT_I18N() \
+  setlocale(LC_MESSAGES, ""); \
+  bindtextdomain("gimp-std-plugins", LOCALEDIR); \
+  textdomain("gimp-std-plugins")
+#define INIT_I18N_UI() \
+  gtk_set_locale(); \
+  setlocale (LC_NUMERIC, "C"); \
+  INIT_I18N();
+#else
+#define INIT_I18N() \
+  bindtextdomain("gimp-std-plugins", LOCALEDIR); \
+  textdomain("gimp-std-plugins")
+#define INIT_I18N_UI() \
+  gtk_set_locale(); \
+  setlocale (LC_NUMERIC, "C"); \
+  INIT_I18N();
+#endif
+#endif
 
 /*
  * Constants for GUI...
