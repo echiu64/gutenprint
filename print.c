2e066a75c5bff4e4a2f93401e478750a8e4a3d49
rlk <>
1999-12-05 22:11:59 +0000

    Increment version

--- print.c
+++ print.c
@@ -49,7 +49,7 @@
  */
 #include <gtk/gtk.h>
 #include <libgimp/gimp.h>
-#define PLUG_IN_VERSION		"3.0 - 22 Nov 1999"
+#define PLUG_IN_VERSION		"3.0.1 - 05 Dec 1999"
 #define PLUG_IN_NAME		"Print"
 
 #include <math.h>
