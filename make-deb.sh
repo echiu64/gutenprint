f53c9332e25765cb2c45fa7a1c8fde76fcd7e38a
sharkey <>
2000-05-13 14:19:21 +0000

    First version of a script for building a debian package.  So far, it only does the ghost script driver, only works for Debian 2.2, and requires the caller to be root.  All of these things can be fixed, but, I'm running late...

---
+++ make-deb.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+set -e
+
+DEBVERSION=`cat /etc/debian_version`
+
+case $DEBVERSION in
+       2.2)
+            make ghost
+            mkdir -p deb
+            cd deb
+            apt-get source gs libjpeg62
+            mv libjpeg6b-6b libjpeg
+            patch -p4 < ../Ghost/debian-patch
+            cd gs-5.10
+            cp ../../Ghost/debian-patch-stp debian/patches/stp
+            mkdir -p contrib/stp
+            cp ../../Ghost/README contrib/stp/README.stp
+            cp ../../Ghost/gdevstp* contrib/stp
+            cp ../../Ghost/devs.mak.addon-5.10 contrib/stp
+            dpkg-buildpackage
+            ;;
+       *)
+            echo "Debian release $DEBVERSION not yet supported."
+            exit 1
+            ;;
+esac
+
+exit 0
