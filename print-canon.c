6b24a361765e6b49e72e7cad8d936d04a96422be
rlk <>
2000-08-11 11:53:22 +0000

    Protect against someone removing code that only looks redundant.

--- print-canon.c
+++ print-canon.c
@@ -905,6 +905,10 @@ canon_print(const printer_t *printer,		/* I - Model */
 			  &orientation, &page_width, &page_height,
 			  &out_width, &out_height, &left, &top);
 
+  /*
+   * Recompute the image height and width.  If the image has been
+   * rotated, these will change from previously.
+   */
   image_height = Image_height(image);
   image_width = Image_width(image);
 
