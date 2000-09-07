08a0222d1339490ae5fd9eb28e5560fbe2af27af
rlk <>
2000-09-07 23:35:12 +0000

    Little fix for the 670

--- print-escp2.c
+++ print-escp2.c
@@ -909,7 +909,7 @@ static escp2_printer_t model_capabilities[] =
      | MODEL_COMMAND_1999 | MODEL_GRAYMODE_YES | MODEL_1440DPI_YES
      | MODEL_ROLLFEED_NO | MODEL_ZEROMARGIN_NO),
     32, 8, 64, 4, 360, INCH(17 / 2), INCH(44), 9, 9, 0, 9, 1, 0,
-    { -1, 3, 0x12, 3, 0x11, -1, 0x10 },
+    { -1, 3, 0x12, 3, 0x11, -1, 0x11 },
     { 2.0, 1.3, 1.3, .646, .710, .323, .365, .1825, .0913 },
     &variable_6pl_4color_inks
   },
