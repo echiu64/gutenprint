6b3ec6d526600fbcfd4e015691a487f2cfc0fe2a
rlk <>
2000-10-02 22:27:41 +0000

    Attempt to fix 115794

--- print-escp2.c
+++ print-escp2.c
@@ -933,8 +933,8 @@ static escp2_printer_t model_capabilities[] =
      | MODEL_6COLOR_NO | MODEL_720DPI_DEFAULT | MODEL_VARIABLE_4
      | MODEL_COMMAND_1999 | MODEL_GRAYMODE_NO | MODEL_1440DPI_NO
      | MODEL_ROLLFEED_NO | MODEL_ZEROMARGIN_NO),
-    15, 8, 15, 8, 720, 720, INCH(17 / 2), INCH(14), 9, 9, 0, 9, 1, 0,
-    { -1, 3, 0x10, 3, 0x10, -1, -1 },
+    15, 6, 15, 6, 360, 360, INCH(17 / 2), INCH(14), 9, 9, 0, 9, 1, 0,
+    { -1, 3, 0x13, 3, 0x10, -1, -1 },
     { 2.0, 1.3, 1.3, .646, .710, .323, .365, .1825, .0913 },
     &variable_6pl_4color_inks
   },
