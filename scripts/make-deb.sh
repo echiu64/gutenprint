864ce5b72b19f35aeb0525c1f40a6100a1ddca03
sharkey <>
2001-02-04 07:31:15 +0000

    Fix paths and targets for new dir structure and build methods.

--- scripts/make-deb.sh
+++ scripts/make-deb.sh
@@ -6,18 +6,20 @@ DEBVERSION=`cat /etc/debian_version`
 
 case $DEBVERSION in
        testing/unstable|woody)
-            make ghost
+            #make ghost
+            ./configure --with-ghost --without-cups --without-gimp
+            make
             mkdir -p deb
             cd deb
             apt-get source gs libjpeg62
             mv libjpeg6b-6b libjpeg
-            patch -p4 < ../Ghost/debian-patch
+            patch -p4 < ../src/ghost/debian-patch
             cd gs-5.10
-            cp ../../Ghost/debian-patch-stp debian/patches/stp
+            cp ../../src/ghost/debian-patch-stp debian/patches/stp
             mkdir -p contrib/stp
-            cp ../../Ghost/README contrib/stp/README.stp
-            cp ../../Ghost/gdevstp* contrib/stp
-            cp ../../Ghost/devs.mak.addon-5.10 contrib/stp
+            cp ../../src/ghost/README contrib/stp/README.stp
+            cp ../../src/ghost/gdevstp* contrib/stp
+            cp ../../src/ghost/devs.mak.addon-5.10 contrib/stp
             fakeroot dpkg-buildpackage
             ;;
        *)
