c6098e06aba56b3108657b8beae94411e37d29b6
rlk <>
2001-02-05 01:07:41 +0000

    STP 2000P is 13" wide, not 8.5 (bug #131044)

--- print-escp2.c
+++ print-escp2.c
@@ -988,7 +988,7 @@ static escp2_printer_t model_capabilities[] =
      | MODEL_6COLOR_YES | MODEL_720DPI_DEFAULT | MODEL_VARIABLE_4
      | MODEL_COMMAND_1999 | MODEL_GRAYMODE_NO | MODEL_1440DPI_YES
      | MODEL_ROLLFEED_NO | MODEL_ZEROMARGIN_NO),
-    48, 6, 144, 2, 360, 360, INCH(17 / 2), INCH(44), 9, 9, 0, 9, 1, 0,
+    48, 6, 144, 2, 360, 360, INCH(13), INCH(44), 9, 9, 0, 9, 1, 0,
     { -1, 2, 0x11, 4, 0x10, -1, 0x10 },
     { 2.0, 1.3, 1.3, .775, .852, .388, .438, .219, .110 },
     &variable_pigment_inks
