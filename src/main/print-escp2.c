23e7686fb1f4625105657dfb6316ac96dba11a29
rlk <>
2001-05-12 14:16:21 +0000

    Tune PGPP

--- src/main/print-escp2.c
+++ src/main/print-escp2.c
@@ -1946,7 +1946,7 @@ static const paper_t escp2_paper_list[] =
   {N_("Inkjet Paper"),7, 0, .78, .25, .6,NULL,plain_paper_lum_adjustment,NULL},
   {N_("Photo Quality Inkjet Paper"), 7, 0, 1, 1.0, .999, NULL, NULL, NULL},
   {N_("Photo Paper"), 8, 0, 1, 1.0, .9, NULL, NULL, NULL},
-  {N_("Premium Glossy Photo Paper"), 8, 0, .9, 1.0, .999, NULL, NULL, NULL},
+  {N_("Premium Glossy Photo Paper"), 8, 0, 1.15, 1.0, .999, NULL, NULL, NULL},
   {N_("Premium Luster Photo Paper"), 8, 0, 1.0, 1.0, .999, NULL, NULL, NULL},
   {N_("Photo Quality Glossy Paper"), 6, 0, 1.0, 1.0, .999, NULL, NULL, NULL},
   {N_("Other"), 0, 0, .7, .125, .5, NULL, plain_paper_lum_adjustment, NULL},
