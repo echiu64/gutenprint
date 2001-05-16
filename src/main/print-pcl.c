272f59465f2067fd294e61b430b52f098abc4936
rlk <>
2001-05-16 00:06:33 +0000

    Fix symbol names

--- src/main/print-pcl.c
+++ src/main/print-pcl.c
@@ -1138,7 +1138,7 @@ static const pcl_cap_t pcl_model_capabilities[] =
 };
 
 
-static const double standard_sat_adjustment[49] =
+static const double sat_adjustment[49] =
 {
   1.0,				/* C */
   1.1,
@@ -1191,7 +1191,7 @@ static const double standard_sat_adjustment[49] =
   1.0				/* C */
 };
 
-static const double standard_lum_adjustment[49] =
+static const double lum_adjustment[49] =
 {
   0.50,				/* C */
   0.6,
@@ -1244,7 +1244,7 @@ static const double standard_lum_adjustment[49] =
   0.50				/* C */
 };
 
-static const double standard_hue_adjustment[49] =
+static const double hue_adjustment[49] =
 {
   0,				/* C */
   0.17,
