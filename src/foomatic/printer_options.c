15c61481eb25df61c4b323fef649fe7a4fa109f4
rlk <>
2001-07-15 00:20:25 +0000

    Fix up options for the current state of the world.

--- src/foomatic/printer_options.c
+++ src/foomatic/printer_options.c
@@ -78,21 +78,32 @@ main(int argc, char **argv)
 	}
       if (tcount > 0)
 	{
+	  printf("$defaults{'%s'}{'%s'} = '%s';\n",
+		 stp_printer_get_driver(p), "Dither",
+		 stp_dither_algorithm_name(0));
+	  for (k = 0; k < stp_dither_algorithm_count(); k++)
+	    printf("$stpdata{'%s'}{'%s'}{'%s'} = 1;\n",
+		   stp_printer_get_driver(p), "Dither",
+		   stp_dither_algorithm_name(k));
 	  if (stp_get_output_type(pv) == OUTPUT_COLOR)
 	    {
 	      printf("$defaults{'%s'}{'%s'} = '%s';\n",
-		     stp_printer_get_driver(p), "Greyscale", "Color");
+		     stp_printer_get_driver(p), "OutputType", "Color");
 	      printf("$stpdata{'%s'}{'%s'}{'%s'} = 1;\n",
-		     stp_printer_get_driver(p), "Greyscale", "Color");
+		     stp_printer_get_driver(p), "OutputType", "Color");
 	      printf("$stpdata{'%s'}{'%s'}{'%s'} = 1;\n",
-		     stp_printer_get_driver(p), "Greyscale", "Greyscale");
+		     stp_printer_get_driver(p), "OutputType", "Grayscale");
+	      printf("$stpdata{'%s'}{'%s'}{'%s'} = 1;\n",
+		     stp_printer_get_driver(p), "OutputType", "BlackAndWhite");
 	    }
 	  else
 	    {
 	      printf("$defaults{'%s'}{'%s'} = '%s';\n",
-		     stp_printer_get_driver(p), "Greyscale", "Greyscale");
+		     stp_printer_get_driver(p), "OutputType", "Grayscale");
 	      printf("$stpdata{'%s'}{'%s'}{'%s'} = 1;\n",
-		     stp_printer_get_driver(p), "Greyscale", "Greyscale");
+		     stp_printer_get_driver(p), "OutputType", "Grayscale");
+	      printf("$stpdata{'%s'}{'%s'}{'%s'} = 1;\n",
+		     stp_printer_get_driver(p), "OutputType", "BlackAndWhite");
 	    }
 	}
     }
