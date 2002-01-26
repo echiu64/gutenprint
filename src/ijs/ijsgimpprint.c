4a511d62dcf7ebdc0526ce561e39b450c67101c1
rlk <>
2002-01-26 13:24:14 +0000

    Note that Russell Lang originally wrote this.

--- src/ijs/ijsgimpprint.c
+++ src/ijs/ijsgimpprint.c
@@ -5,6 +5,8 @@
  *
  *   Copyright 2001 Robert Krawitz (rlk@alum.mit.edu)
  *
+ *   Originally written by Russell Lang, copyright assigned to Robert Krawitz.
+ *
  *   This program is free software; you can redistribute it and/or modify it
  *   under the terms of the GNU General Public License as published by the Free
  *   Software Foundation; either version 2 of the License, or (at your option)
