f620b48ea97e4d04eaef5958d2b5664269e6bff9
rlk <>
2002-11-07 12:00:52 +0000

    631993 Macintosh OS X: Custom page size problems w/some apps

--- src/cups/genppd.c
+++ src/cups/genppd.c
@@ -520,7 +520,7 @@ write_ppd(const stp_printer_t p,	/* I - Printer driver */
     cur_opt++;
   }
 
-  gzprintf(fp, "*VariableSizes: %s\n\n", variable_sizes ? "true" : "false");
+  gzprintf(fp, "*VariablePaperSize: %s\n\n", variable_sizes ? "true" : "false");
 
   gzputs(fp, "*OpenUI *PageSize: PickOne\n");
   gzputs(fp, "*OrderDependency: 10 AnySetup *PageSize\n");
