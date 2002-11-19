fbc8d66e9af5560584172674e998b7f9a1ff22ce
rleigh <>
2002-11-19 23:03:28 +0000

    printdef uses libxml2 to read printers.xml Make printers.xml into valid XML, using gimp-print schema Add configure.ac check for libxml2

--- src/printdef/printdef.h
+++ src/printdef/printdef.h
@@ -1,10 +1,11 @@
 /*
  * "$Id$"
  *
- *   I18N header file for the gimp-print plugin.
+ *   printdef XML parser header
  *
  *   Copyright 1997-2000 Michael Sweet (mike@easysw.com),
  *	Robert Krawitz (rlk@alum.mit.edu) and Michael Natterer (mitch@gimp.org)
+ *   Copyright 2002 Roger Leigh (roger@whinlatter.uklinux.net)
  *
  *   This program is free software; you can redistribute it and/or modify it
  *   under the terms of the GNU General Public License as published by the Free
@@ -21,81 +22,14 @@
  *   Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
  */
 
-#define OUTPUT_GRAY		0	/* Grayscale output */
-#define OUTPUT_COLOR		1	/* Color output */
-#define OUTPUT_GRAY_COLOR	2 	/* Grayscale output using color */
 
-#define ORIENT_AUTO		-1	/* Best orientation */
-#define ORIENT_PORTRAIT		0	/* Portrait orientation */
-#define ORIENT_LANDSCAPE	1	/* Landscape orientation */
-#define ORIENT_UPSIDEDOWN	2	/* Reverse portrait orientation */
-#define ORIENT_SEASCAPE		3	/* Reverse landscape orientation */
-
-#define IMAGE_LINE_ART		0
-#define IMAGE_SOLID_TONE	1
-#define IMAGE_CONTINUOUS	2
-#define IMAGE_MONOCHROME	3
-#define NIMAGE_TYPES		4
-
-#define COLOR_MODEL_RGB		0
-#define COLOR_MODEL_CMY		1
-
-typedef struct					/* Plug-in variables */
-{
-  char	output_to[256],		/* Name of file or command to print to */
-	driver[64],		/* Name of printer "driver" */
-	ppd_file[256],		/* PPD file */
-	resolution[64],		/* Resolution */
-	media_size[64],		/* Media size */
-	media_type[64],		/* Media type */
-	media_source[64],	/* Media source */
-	ink_type[64],		/* Ink or cartridge */
-	dither_algorithm[64];	/* Dithering algorithm */
-  int	output_type;		/* Color or grayscale output */
-  float	brightness;		/* Output brightness */
-  int	left,			/* Offset from left-upper corner, points */
-	top,			/* ... */
-        width,			/* Width of the image, points */
-	height;			/* ... */
-  float gamma;                  /* Gamma */
-  float contrast,		/* Output Contrast */
-	cyan,			/* Output red level */
-	magenta,		/* Output green level */
-	yellow;			/* Output blue level */
-  float	saturation;		/* Output saturation */
-  float	density;		/* Maximum output density */
-  int	image_type;		/* Image type (line art etc.) */
-  float app_gamma;		/* Application gamma */
-  int	page_width;		/* Width of page in points */
-  int	page_height;		/* Height of page in points */
-  int	input_color_model;	/* Color model for this device */
-  int	output_color_model;	/* Color model for this device */
-  void  *lut;			/* Look-up table */
-  void  *driver_data;		/* Private data of the driver */
-  unsigned char *cmap;		/* Color map */
-  void (*outfunc)(void *data, const char *buffer, size_t bytes);
-  void *outdata;
-  void (*errfunc)(void *data, const char *buffer, size_t bytes);
-  void *errdata;
-} stp_vars_t;
-
-typedef struct stp_printer
+typedef struct stp_printdef_printer
 {
   const char	*long_name,			/* Long name for UI */
-	*driver;			/* Short name for printrc file */
-  int	model;				/* Model number */
-  const char *printfuncs;
-  stp_vars_t printvars;
-} stp_printer_t;
-
-typedef union yylv {
-  int ival;
-  double dval;
-  char *sval;
-} YYSTYPE;
-
-extern YYSTYPE yylval;
-extern stp_printer_t thePrinter;
-
-#include "printdefy.h"
+	        *driver,			/* Short name for printrc file */
+                *family;                        /* Family driver name */
+  int	        model;				/* Model number */
+  const char    *printfuncs;
+  stp_internal_vars_t printvars;
+} stp_printdef_printer_t;
 
