433f488dd2d6fca49bd401d8d359beee1d0419b4
rlk <>
2002-12-07 16:31:26 +0000

    2200 knows about gray command

--- src/main/print-escp2-data.c
+++ src/main/print-escp2-data.c
@@ -4879,7 +4879,7 @@ const escp2_stp_printer_t stp_escp2_model_capabilities[] =
   },
   /* 47: Stylus Photo 2100/2200 */
   {
-    (MODEL_VARIABLE_YES | MODEL_COMMAND_2000 | MODEL_GRAYMODE_NO |
+    (MODEL_VARIABLE_YES | MODEL_COMMAND_2000 | MODEL_GRAYMODE_YES |
      MODEL_ROLLFEED_YES | MODEL_XZEROMARGIN_YES | MODEL_VACUUM_NO |
      MODEL_FAST_360_YES),
     96, 1, 2, 96, 1, 2, 192, 1, 1, 7,
