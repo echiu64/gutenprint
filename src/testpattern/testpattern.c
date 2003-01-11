9ac1e9fbd49597c0f428b9c59f2efadbb380fceb
rlk <>
2003-01-11 15:59:28 +0000

    Valgrind cleanup

--- src/testpattern/testpattern.c
+++ src/testpattern/testpattern.c
@@ -276,7 +276,7 @@ main(int argc, char **argv)
 	  levels = atoi(optarg);
 	  break;
 	case 'p':
-	  printer = c_strdup(optarg);
+	  printer = optarg;
 	  break;
 	case 'q':
 	  noblackline = 1;
@@ -396,6 +396,7 @@ main(int argc, char **argv)
   if (stp_print(v, &theImage) != 1)
     return 1;
   stp_vars_free(v);
+  free(the_testpatterns);
   return 0;
 }
 
