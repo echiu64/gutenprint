6c2d90aff28a1d503f9330c540c2bac86004bf9a
rlk <>
2003-06-04 11:55:10 +0000

    Fix densities for 2200

--- src/main/print-escp2-data.c
+++ src/main/print-escp2-data.c
@@ -258,7 +258,7 @@ static const escp2_densities_t c1_8pl_densities =
 { 2.3, 1.15, 0.57, 0.650, 0.650,     0, 0.650, 0.360 };
 
 static const escp2_densities_t c4pl_pigment_densities =
-{ 2.7, 1.35, 0.68, 0.518, 0.518, 0.518, 0.363, 0.182 };
+{ 2.7, 1.35, 0.68, 0.725, 0.725, 0.725, 0.363, 0.182 };
 
 
 static const input_slot_t standard_roll_feed_input_slots[] =
