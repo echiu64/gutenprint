3d6d139954b0add5a1eac77e0e2adb60572c0f0b
rlk <>
2003-07-17 11:47:29 +0000

    Print out origin of all messages

--- src/cups/rastertoprinter.c
+++ src/cups/rastertoprinter.c
@@ -133,7 +133,7 @@ set_special_parameter(stp_vars_t v, const char *name, int choice)
   if (desc.p_type == STP_PARAMETER_TYPE_STRING_LIST)
     {
       if (choice >= stp_string_list_count(desc.bounds.str))
-	fprintf(stderr, "ERROR: Unable to set %s!\n", name);
+	fprintf(stderr, "ERROR: Gimp-Print unable to set %s!\n", name);
       else
 	stp_set_string_parameter
 	  (v, name, stp_string_list_param(desc.bounds.str, choice)->name);
@@ -144,52 +144,52 @@ set_special_parameter(stp_vars_t v, const char *name, int choice)
 static void
 print_debug_block(cups_image_t *cups)
 {
-  fprintf(stderr, "DEBUG: StartPage...\n");
-  fprintf(stderr, "DEBUG: MediaClass = \"%s\"\n", cups->header.MediaClass);
-  fprintf(stderr, "DEBUG: MediaColor = \"%s\"\n", cups->header.MediaColor);
-  fprintf(stderr, "DEBUG: MediaType = \"%s\"\n", cups->header.MediaType);
-  fprintf(stderr, "DEBUG: OutputType = \"%s\"\n", cups->header.OutputType);
+  fprintf(stderr, "DEBUG: Gimp-Print StartPage...\n");
+  fprintf(stderr, "DEBUG: Gimp-Print MediaClass = \"%s\"\n", cups->header.MediaClass);
+  fprintf(stderr, "DEBUG: Gimp-Print MediaColor = \"%s\"\n", cups->header.MediaColor);
+  fprintf(stderr, "DEBUG: Gimp-Print MediaType = \"%s\"\n", cups->header.MediaType);
+  fprintf(stderr, "DEBUG: Gimp-Print OutputType = \"%s\"\n", cups->header.OutputType);
 
-  fprintf(stderr, "DEBUG: AdvanceDistance = %d\n", cups->header.AdvanceDistance);
-  fprintf(stderr, "DEBUG: AdvanceMedia = %d\n", cups->header.AdvanceMedia);
-  fprintf(stderr, "DEBUG: Collate = %d\n", cups->header.Collate);
-  fprintf(stderr, "DEBUG: CutMedia = %d\n", cups->header.CutMedia);
-  fprintf(stderr, "DEBUG: Duplex = %d\n", cups->header.Duplex);
-  fprintf(stderr, "DEBUG: HWResolution = [ %d %d ]\n", cups->header.HWResolution[0],
+  fprintf(stderr, "DEBUG: Gimp-Print AdvanceDistance = %d\n", cups->header.AdvanceDistance);
+  fprintf(stderr, "DEBUG: Gimp-Print AdvanceMedia = %d\n", cups->header.AdvanceMedia);
+  fprintf(stderr, "DEBUG: Gimp-Print Collate = %d\n", cups->header.Collate);
+  fprintf(stderr, "DEBUG: Gimp-Print CutMedia = %d\n", cups->header.CutMedia);
+  fprintf(stderr, "DEBUG: Gimp-Print Duplex = %d\n", cups->header.Duplex);
+  fprintf(stderr, "DEBUG: Gimp-Print HWResolution = [ %d %d ]\n", cups->header.HWResolution[0],
 	  cups->header.HWResolution[1]);
-  fprintf(stderr, "DEBUG: ImagingBoundingBox = [ %d %d %d %d ]\n",
+  fprintf(stderr, "DEBUG: Gimp-Print ImagingBoundingBox = [ %d %d %d %d ]\n",
 	  cups->header.ImagingBoundingBox[0], cups->header.ImagingBoundingBox[1],
 	  cups->header.ImagingBoundingBox[2], cups->header.ImagingBoundingBox[3]);
-  fprintf(stderr, "DEBUG: InsertSheet = %d\n", cups->header.InsertSheet);
-  fprintf(stderr, "DEBUG: Jog = %d\n", cups->header.Jog);
-  fprintf(stderr, "DEBUG: LeadingEdge = %d\n", cups->header.LeadingEdge);
-  fprintf(stderr, "DEBUG: Margins = [ %d %d ]\n", cups->header.Margins[0],
+  fprintf(stderr, "DEBUG: Gimp-Print InsertSheet = %d\n", cups->header.InsertSheet);
+  fprintf(stderr, "DEBUG: Gimp-Print Jog = %d\n", cups->header.Jog);
+  fprintf(stderr, "DEBUG: Gimp-Print LeadingEdge = %d\n", cups->header.LeadingEdge);
+  fprintf(stderr, "DEBUG: Gimp-Print Margins = [ %d %d ]\n", cups->header.Margins[0],
 	  cups->header.Margins[1]);
-  fprintf(stderr, "DEBUG: ManualFeed = %d\n", cups->header.ManualFeed);
-  fprintf(stderr, "DEBUG: MediaPosition = %d\n", cups->header.MediaPosition);
-  fprintf(stderr, "DEBUG: MediaWeight = %d\n", cups->header.MediaWeight);
-  fprintf(stderr, "DEBUG: MirrorPrint = %d\n", cups->header.MirrorPrint);
-  fprintf(stderr, "DEBUG: NegativePrint = %d\n", cups->header.NegativePrint);
-  fprintf(stderr, "DEBUG: NumCopies = %d\n", cups->header.NumCopies);
-  fprintf(stderr, "DEBUG: Orientation = %d\n", cups->header.Orientation);
-  fprintf(stderr, "DEBUG: OutputFaceUp = %d\n", cups->header.OutputFaceUp);
-  fprintf(stderr, "DEBUG: PageSize = [ %d %d ]\n", cups->header.PageSize[0],
+  fprintf(stderr, "DEBUG: Gimp-Print ManualFeed = %d\n", cups->header.ManualFeed);
+  fprintf(stderr, "DEBUG: Gimp-Print MediaPosition = %d\n", cups->header.MediaPosition);
+  fprintf(stderr, "DEBUG: Gimp-Print MediaWeight = %d\n", cups->header.MediaWeight);
+  fprintf(stderr, "DEBUG: Gimp-Print MirrorPrint = %d\n", cups->header.MirrorPrint);
+  fprintf(stderr, "DEBUG: Gimp-Print NegativePrint = %d\n", cups->header.NegativePrint);
+  fprintf(stderr, "DEBUG: Gimp-Print NumCopies = %d\n", cups->header.NumCopies);
+  fprintf(stderr, "DEBUG: Gimp-Print Orientation = %d\n", cups->header.Orientation);
+  fprintf(stderr, "DEBUG: Gimp-Print OutputFaceUp = %d\n", cups->header.OutputFaceUp);
+  fprintf(stderr, "DEBUG: Gimp-Print PageSize = [ %d %d ]\n", cups->header.PageSize[0],
 	  cups->header.PageSize[1]);
-  fprintf(stderr, "DEBUG: Separations = %d\n", cups->header.Separations);
-  fprintf(stderr, "DEBUG: TraySwitch = %d\n", cups->header.TraySwitch);
-  fprintf(stderr, "DEBUG: Tumble = %d\n", cups->header.Tumble);
-  fprintf(stderr, "DEBUG: cupsWidth = %d\n", cups->header.cupsWidth);
-  fprintf(stderr, "DEBUG: cupsHeight = %d\n", cups->header.cupsHeight);
-  fprintf(stderr, "DEBUG: cupsMediaType = %d\n", cups->header.cupsMediaType);
-  fprintf(stderr, "DEBUG: cupsBitsPerColor = %d\n", cups->header.cupsBitsPerColor);
-  fprintf(stderr, "DEBUG: cupsBitsPerPixel = %d\n", cups->header.cupsBitsPerPixel);
-  fprintf(stderr, "DEBUG: cupsBytesPerLine = %d\n", cups->header.cupsBytesPerLine);
-  fprintf(stderr, "DEBUG: cupsColorOrder = %d\n", cups->header.cupsColorOrder);
-  fprintf(stderr, "DEBUG: cupsColorSpace = %d\n", cups->header.cupsColorSpace);
-  fprintf(stderr, "DEBUG: cupsCompression = %d\n", cups->header.cupsCompression);
-  fprintf(stderr, "DEBUG: cupsRowCount = %d\n", cups->header.cupsRowCount);
-  fprintf(stderr, "DEBUG: cupsRowFeed = %d\n", cups->header.cupsRowFeed);
-  fprintf(stderr, "DEBUG: cupsRowStep = %d\n", cups->header.cupsRowStep);
+  fprintf(stderr, "DEBUG: Gimp-Print Separations = %d\n", cups->header.Separations);
+  fprintf(stderr, "DEBUG: Gimp-Print TraySwitch = %d\n", cups->header.TraySwitch);
+  fprintf(stderr, "DEBUG: Gimp-Print Tumble = %d\n", cups->header.Tumble);
+  fprintf(stderr, "DEBUG: Gimp-Print cupsWidth = %d\n", cups->header.cupsWidth);
+  fprintf(stderr, "DEBUG: Gimp-Print cupsHeight = %d\n", cups->header.cupsHeight);
+  fprintf(stderr, "DEBUG: Gimp-Print cupsMediaType = %d\n", cups->header.cupsMediaType);
+  fprintf(stderr, "DEBUG: Gimp-Print cupsBitsPerColor = %d\n", cups->header.cupsBitsPerColor);
+  fprintf(stderr, "DEBUG: Gimp-Print cupsBitsPerPixel = %d\n", cups->header.cupsBitsPerPixel);
+  fprintf(stderr, "DEBUG: Gimp-Print cupsBytesPerLine = %d\n", cups->header.cupsBytesPerLine);
+  fprintf(stderr, "DEBUG: Gimp-Print cupsColorOrder = %d\n", cups->header.cupsColorOrder);
+  fprintf(stderr, "DEBUG: Gimp-Print cupsColorSpace = %d\n", cups->header.cupsColorSpace);
+  fprintf(stderr, "DEBUG: Gimp-Print cupsCompression = %d\n", cups->header.cupsCompression);
+  fprintf(stderr, "DEBUG: Gimp-Print cupsRowCount = %d\n", cups->header.cupsRowCount);
+  fprintf(stderr, "DEBUG: Gimp-Print cupsRowFeed = %d\n", cups->header.cupsRowFeed);
+  fprintf(stderr, "DEBUG: Gimp-Print cupsRowStep = %d\n", cups->header.cupsRowStep);
 }
 
 static stp_vars_t
@@ -223,7 +223,7 @@ initialize_page(cups_image_t *cups, stp_const_vars_t default_settings)
       stp_set_output_type(v, OUTPUT_RAW_CMYK);
       break;
     default :
-      fprintf(stderr, "ERROR: Bad colorspace %d!",
+      fprintf(stderr, "ERROR: Gimp-Print Bad colorspace %d!",
 	      cups->header.cupsColorSpace);
       break;
     }
@@ -233,14 +233,14 @@ initialize_page(cups_image_t *cups, stp_const_vars_t default_settings)
   stp_set_string_parameter(v, "InputSlot", cups->header.MediaClass);
   stp_set_string_parameter(v, "MediaType", cups->header.MediaType);
 
-  fprintf(stderr, "DEBUG: PageSize = %dx%d\n", cups->header.PageSize[0],
+  fprintf(stderr, "DEBUG: Gimp-Print PageSize = %dx%d\n", cups->header.PageSize[0],
 	  cups->header.PageSize[1]);
 
   if ((size = stp_get_papersize_by_size(cups->header.PageSize[1],
 					cups->header.PageSize[0])) != NULL)
     stp_set_string_parameter(v, "PageSize", size->name);
   else
-    fprintf(stderr, "ERROR: Unable to get media size!\n");
+    fprintf(stderr, "ERROR: Gimp-Print Unable to get media size!\n");
 
 
   params = stp_get_parameter_list(v);
@@ -251,23 +251,23 @@ initialize_page(cups_image_t *cups, stp_const_vars_t default_settings)
       switch (p->p_type)
 	{
 	case STP_PARAMETER_TYPE_STRING_LIST:
-	  fprintf(stderr, "DEBUG: stp_get_string %s(v) |%s| %d\n",
+	  fprintf(stderr, "DEBUG: Gimp-Print stp_get_string %s(v) |%s| %d\n",
 		  p->name, stp_get_string_parameter(v, p->name) ?
 		  stp_get_string_parameter(v, p->name) : "NULL",
 		  stp_get_string_parameter_active(v, p->name));
 	  break;
 	case STP_PARAMETER_TYPE_DOUBLE:
-	  fprintf(stderr, "DEBUG: stp_get_float %s(v) |%.3f| %d\n",
+	  fprintf(stderr, "DEBUG: Gimp-Print stp_get_float %s(v) |%.3f| %d\n",
 		  p->name, stp_get_float_parameter(v, p->name),
 		  stp_get_float_parameter_active(v, p->name));
 	  break;
 	case STP_PARAMETER_TYPE_INT:
-	  fprintf(stderr, "DEBUG: stp_get_int %s(v) |%.d| %d\n",
+	  fprintf(stderr, "DEBUG: Gimp-Print stp_get_int %s(v) |%.d| %d\n",
 		  p->name, stp_get_int_parameter(v, p->name),
 		  stp_get_int_parameter_active(v, p->name));
 	  break;
 	case STP_PARAMETER_TYPE_BOOLEAN:
-	  fprintf(stderr, "DEBUG: stp_get_boolean %s(v) |%.d| %d\n",
+	  fprintf(stderr, "DEBUG: Gimp-Print stp_get_boolean %s(v) |%.d| %d\n",
 		  p->name, stp_get_boolean_parameter(v, p->name),
 		  stp_get_boolean_parameter_active(v, p->name));
 	  break;
@@ -280,17 +280,17 @@ initialize_page(cups_image_t *cups, stp_const_vars_t default_settings)
     }
   stp_parameter_list_free(params);
   stp_set_job_mode(v, STP_JOB_MODE_JOB);
-  fprintf(stderr, "DEBUG: stp_get_driver(v) |%s|\n", stp_get_driver(v));
-  fprintf(stderr, "DEBUG: stp_get_output_type(v) |%d|\n", stp_get_output_type(v));
-  fprintf(stderr, "DEBUG: stp_get_left(v) |%d|\n", stp_get_left(v));
-  fprintf(stderr, "DEBUG: stp_get_top(v) |%d|\n", stp_get_top(v));
-  fprintf(stderr, "DEBUG: stp_get_page_width(v) |%d|\n", stp_get_page_width(v));
-  fprintf(stderr, "DEBUG: stp_get_page_height(v) |%d|\n", stp_get_page_height(v));
-  fprintf(stderr, "DEBUG: stp_get_input_color_model(v) |%d|\n", stp_get_input_color_model(v));
+  fprintf(stderr, "DEBUG: Gimp-Print stp_get_driver(v) |%s|\n", stp_get_driver(v));
+  fprintf(stderr, "DEBUG: Gimp-Print stp_get_output_type(v) |%d|\n", stp_get_output_type(v));
+  fprintf(stderr, "DEBUG: Gimp-Print stp_get_left(v) |%d|\n", stp_get_left(v));
+  fprintf(stderr, "DEBUG: Gimp-Print stp_get_top(v) |%d|\n", stp_get_top(v));
+  fprintf(stderr, "DEBUG: Gimp-Print stp_get_page_width(v) |%d|\n", stp_get_page_width(v));
+  fprintf(stderr, "DEBUG: Gimp-Print stp_get_page_height(v) |%d|\n", stp_get_page_height(v));
+  fprintf(stderr, "DEBUG: Gimp-Print stp_get_input_color_model(v) |%d|\n", stp_get_input_color_model(v));
   stp_get_media_size(v, &(cups->width), &(cups->height));
   stp_get_imageable_area(v, &(cups->left), &(cups->right),
 			 &(cups->bottom), &(cups->top));
-  fprintf(stderr, "DEBUG: GIMP-PRINT %d %d %d  %d %d %d\n",
+  fprintf(stderr, "DEBUG: Gimp-Print %d %d %d  %d %d %d\n",
 	  cups->width, cups->left, cups->right, cups->height, cups->top, cups->bottom);
   stp_set_width(v, cups->right - cups->left);
   stp_set_height(v, cups->bottom - cups->top);
@@ -307,7 +307,7 @@ initialize_page(cups_image_t *cups, stp_const_vars_t default_settings)
   cups->height = cups->header.HWResolution[1] * cups->height / 72;
   cups->top = cups->header.HWResolution[1] * cups->top / 72;
   cups->bottom = cups->header.HWResolution[1] * cups->bottom / 72;
-  fprintf(stderr, "DEBUG: GIMP-PRINT %d %d %d  %d %d %d\n",
+  fprintf(stderr, "DEBUG: Gimp-Print %d %d %d  %d %d %d\n",
 	  cups->width, cups->left, cups->right, cups->height, cups->top, cups->bottom);
 
   return v;
@@ -318,12 +318,16 @@ purge_excess_data(cups_image_t *cups)
 {
   char *buffer = xmalloc(cups->header.cupsBytesPerLine);
   if (buffer)
-    while (cups->row < cups->header.cupsHeight)
-      {
-	cupsRasterReadPixels(cups->ras, (unsigned char *)buffer,
-			     cups->header.cupsBytesPerLine);
-	cups->row ++;
-      }
+    {
+      fprintf(stderr, "DEBUG: Gimp-Print purging %d rows\n",
+	      cups->header.cupsHeight - cups->row);
+      while (cups->row < cups->header.cupsHeight)
+	{
+	  cupsRasterReadPixels(cups->ras, (unsigned char *)buffer,
+			       cups->header.cupsBytesPerLine);
+	  cups->row ++;
+	}
+    }
   free(buffer);
 }
 
@@ -410,6 +414,8 @@ set_all_options(stp_vars_t v, cups_option_t *options, int num_options,
 		case STP_PARAMETER_TYPE_CURVE: /* figure this out later... */
 		case STP_PARAMETER_TYPE_FILE: /* Probably not, security hole */
 		case STP_PARAMETER_TYPE_RAW: /* figure this out later, too */
+		  fprintf(stderr, "DEBUG: Gimp-Print ignoring option %s %s type %d\n",
+			  desc.name, val, desc.p_type);
 		  break;
 		default:
 		  break;
@@ -460,7 +466,7 @@ main(int  argc,				/* I - Number of command-line arguments */
     * and return.
     */
 
-    fputs("ERROR: rastertoprinter job-id user title copies options [file]\n", stderr);
+    fputs("ERROR: Gimp-Print rastertoprinter job-id user title copies options [file]\n", stderr);
     return (1);
   }
 
@@ -470,20 +476,20 @@ main(int  argc,				/* I - Number of command-line arguments */
 
   if ((ppdfile = getenv("PPD")) == NULL)
   {
-    fputs("ERROR: Fatal error: PPD environment variable not set!\n", stderr);
+    fputs("ERROR: Gimp-Print Fatal error: PPD environment variable not set!\n", stderr);
     return (1);
   }
 
   if ((ppd = ppdOpenFile(ppdfile)) == NULL)
   {
-    fprintf(stderr, "ERROR: Fatal error: Unable to load PPD file \"%s\"!\n",
+    fprintf(stderr, "ERROR: Gimp-Print Fatal error: Unable to load PPD file \"%s\"!\n",
             ppdfile);
     return (1);
   }
 
   if (ppd->modelname == NULL)
   {
-    fprintf(stderr, "ERROR: Fatal error: No ModelName attribute in PPD file \"%s\"!\n",
+    fprintf(stderr, "ERROR: Gimp-Print Fatal error: No ModelName attribute in PPD file \"%s\"!\n",
             ppdfile);
     ppdClose(ppd);
     return (1);
@@ -493,9 +499,18 @@ main(int  argc,				/* I - Number of command-line arguments */
   * Get the STP options, if any...
   */
 
-  fprintf(stderr, "DEBUG: Gimp-Print options are %s\n", argv[5]);
-
   num_options = cupsParseOptions(argv[5], 0, &options);
+
+  fprintf(stderr, "DEBUG: Gimp-Print CUPS option count is %d (%d bytes)\n",
+	  num_options, strlen(argv[5]));
+
+  if (num_options > 0)
+    {
+      int i;
+      for (i = 0; i < num_options; i++)
+	fprintf(stderr, "DEBUG: Gimp-Print    CUPS option %d %s = %s\n",
+		i, options[i].name, options[i].value);
+    }
 
  /*
   * Figure out which driver to use...
@@ -504,7 +519,7 @@ main(int  argc,				/* I - Number of command-line arguments */
   if ((printer = stp_get_printer_by_long_name(ppd->modelname)) == NULL)
     if ((printer = stp_get_printer_by_driver(ppd->modelname)) == NULL)
     {
-      fprintf(stderr, "ERROR: Fatal error: Unable to find driver named \"%s\"!\n",
+      fprintf(stderr, "ERROR: Gimp-Print Fatal error: Unable to find driver named \"%s\"!\n",
               ppd->modelname);
       ppdClose(ppd);
       return (1);
@@ -518,7 +533,7 @@ main(int  argc,				/* I - Number of command-line arguments */
   {
     if ((fd = open(argv[6], O_RDONLY)) == -1)
     {
-      perror("ERROR: Unable to open raster file - ");
+      perror("ERROR: Gimp-Print Unable to open raster file - ");
       sleep(1);
       return (1);
     }
@@ -539,6 +554,8 @@ main(int  argc,				/* I - Number of command-line arguments */
   */
 
   cups.page = 0;
+
+  fprintf(stderr, "DEBUG: Gimp-Print about to start printing loop.\n");
 
   /*
    * Read the first page header, which we need in order to set up
@@ -564,10 +581,14 @@ main(int  argc,				/* I - Number of command-line arguments */
       v = initialize_page(&cups, default_settings);
       stp_set_page_number(v, cups.page);
       cups.row = 0;
+      fprintf(stderr, "DEBUG: Gimp-Print printing page %d\n", cups.page);
       fprintf(stderr, "PAGE: %d 1\n", cups.page);
       print_debug_block(&cups);
       if (!stp_verify(v))
-        goto cups_abort;
+	{
+	  fprintf(stderr, "ERROR: Gimp-Print options failed to verify, set LogLevel to debug2 to identify why\n");
+	  goto cups_abort;
+	}
 
       if (!initialized_job)
 	{
@@ -576,7 +597,10 @@ main(int  argc,				/* I - Number of command-line arguments */
 	}
 
       if (!stp_print(v, &theImage))
-        goto cups_abort;
+	{
+	  fprintf(stderr, "ERROR: Gimp-Print failed to print, set LogLevel to debug2 to identify why\n");
+	  goto cups_abort;
+	}
 
       fflush(stdout);
 
@@ -589,23 +613,24 @@ main(int  argc,				/* I - Number of command-line arguments */
     }
   if (v)
     {
+      fprintf(stderr, "DEBUG: Gimp-Print ending job\n");
       stp_end_job(v, &theImage);
       stp_vars_free(v);
     }
   cupsRasterClose(cups.ras);
   if (fd != 0)
     close(fd);
-  fputs("INFO: Ready to print.\n", stderr);
+  fputs("INFO: Gimp-Print Ready to print.\n", stderr);
   return 0;
 
 cups_abort:
-  fputs("ERROR: Invalid printer settings!\n", stderr);
+  fputs("ERROR: Gimp-Print Invalid printer settings!\n", stderr);
   stp_end_job(v, &theImage);
   stp_vars_free(v);
   cupsRasterClose(cups.ras);
   if (fd != 0)
     close(fd);
-  fputs("ERROR: No pages found!\n", stderr);
+  fputs("ERROR: Gimp-Print No pages found!\n", stderr);
   return 1;
 }
 
@@ -714,7 +739,7 @@ Image_get_row(stp_image_t   *image,	/* I - Image */
 
   if (cups->row < cups->header.cupsHeight)
   {
-    fprintf(stderr, "DEBUG2: GIMP-PRINT reading %d %d\n",
+    fprintf(stderr, "DEBUG2: Gimp-Print reading %d %d\n",
 	    bytes_per_line, cups->row);
     while (cups->row < row)
       {
@@ -722,7 +747,7 @@ Image_get_row(stp_image_t   *image,	/* I - Image */
 	cups->row ++;
 	if (margin > 0)
 	  {
-	    fprintf(stderr, "DEBUG2: GIMP-PRINT tossing right %d\n", margin);
+	    fprintf(stderr, "DEBUG2: Gimp-Print tossing right %d\n", margin);
 	    throwaway_data(margin, cups);
 	  }
       }
@@ -761,7 +786,7 @@ Image_height(stp_image_t *image)	/* I - Image */
   if ((cups = (cups_image_t *)(image->rep)) == NULL)
     return (0);
 
-  fprintf(stderr, "DEBUG: GIMP-PRINT: Image_height %d\n", cups->height);
+  fprintf(stderr, "DEBUG: Gimp-Print: Image_height %d\n", cups->height);
   return (cups->height);
 }
 
@@ -790,7 +815,7 @@ Image_note_progress(stp_image_t *image,	/* I - Image */
   if ((cups = (cups_image_t *)(image->rep)) == NULL)
     return;
 
-  fprintf(stderr, "INFO: Printing page %d, %.0f%%\n",
+  fprintf(stderr, "INFO: Gimp-Print Printing page %d, %.0f%%\n",
           cups->page, 100.0 * current / total);
 }
 
@@ -807,7 +832,7 @@ Image_progress_conclude(stp_image_t *image)	/* I - Image */
   if ((cups = (cups_image_t *)(image->rep)) == NULL)
     return;
 
-  fprintf(stderr, "INFO: Finished page %d...\n", cups->page);
+  fprintf(stderr, "INFO: Gimp-Print Finished page %d...\n", cups->page);
 }
 
 /*
@@ -823,7 +848,7 @@ Image_progress_init(stp_image_t *image)/* I - Image */
   if ((cups = (cups_image_t *)(image->rep)) == NULL)
     return;
 
-  fprintf(stderr, "INFO: Starting page %d...\n", cups->page);
+  fprintf(stderr, "INFO: Gimp-Print Starting page %d...\n", cups->page);
 }
 
 /*
@@ -839,7 +864,7 @@ Image_width(stp_image_t *image)	/* I - Image */
   if ((cups = (cups_image_t *)(image->rep)) == NULL)
     return (0);
 
-  fprintf(stderr, "DEBUG: GIMP-PRINT: Image_width %d\n", cups->width);
+  fprintf(stderr, "DEBUG: Gimp-Print: Image_width %d\n", cups->width);
   return (cups->width);
 }
 
