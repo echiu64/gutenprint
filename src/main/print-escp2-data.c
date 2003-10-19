5990dcd89029513d28172f228eb28f1bcd480cfb
rlk <>
2003-10-19 14:24:16 +0000

    Per spec sheet SC1520 has 128 black nozzles.

--- src/main/print-escp2-data.c
+++ src/main/print-escp2-data.c
@@ -533,7 +533,7 @@ const stpi_escp2_printer_t stpi_escp2_model_capabilities[] =
      MODEL_ROLLFEED_YES | MODEL_XZEROMARGIN_NO | MODEL_VACUUM_NO |
      MODEL_FAST_360_NO | MODEL_SEND_ZERO_ADVANCE_YES |
      MODEL_SUPPORTS_INK_CHANGE_NO),
-    64, 1, 2, 64, 1, 2, 64, 1, 2, 4,
+    64, 1, 2, 128, 1, 1, 64, 1, 2, 4,
     360, 14400, -1, 1440, 720, 90, 90, 0, 1, 4, 0, 0, 0, 0,
     INCH(17), INCH(44), INCH(2), INCH(4),
     8, 9, 9, 40, 8, 9, 9, 40, 8, 9, 0, 0, 8, 9, 0, 0,
