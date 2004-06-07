0a0ae88bd3db522aaffbc2cab071b94e703d8880
rlk <>
2004-06-07 01:49:57 +0000

    Resample the gcr curve correctly.

--- src/main/color-conversions.c
+++ src/main/color-conversions.c
@@ -415,6 +415,7 @@ generic_cmy_to_kcmy(const stp_vars_t *vars, const unsigned short *in,
   unsigned short nz2 = 0;
   unsigned short nz3 = 0;
 
+  stp_curve_resample(stp_curve_cache_get_curve(&(lut->gcr_curve)), lut->steps);
   gcr_lookup = stp_curve_cache_get_ushort_data(&(lut->gcr_curve));
   stp_curve_resample(stp_curve_cache_get_curve
 		     (&(lut->channel_curves[CHANNEL_K])), lut->steps);
