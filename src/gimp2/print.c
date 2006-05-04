7aca6434475b2d36b5734da0e3bfe11c6d6e2169
rlk <>
2006-05-04 11:10:58 +0000

    Warn translators not to translate the top level part of the menu.

--- src/gimp2/print.c
+++ src/gimp2/print.c
@@ -133,6 +133,7 @@ query (void)
 			  (BAD_CONST_CHAR) auth,
 			  (BAD_CONST_CHAR) copy,
 			  (BAD_CONST_CHAR) VERSION " - " RELEASE_DATE,
+			  /* Do not translate the prefix "<Image>" */
 			  (BAD_CONST_CHAR) N_("<Image>/File/Print..."),
 			  (BAD_CONST_CHAR) types,
 			  GIMP_PLUGIN,
