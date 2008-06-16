961d04cd8a8c1fb0cf53b54fcc130d162219c8ab
rlk <>
2008-06-16 02:21:10 +0000

    Remove safety check for output order, which is really implemented in CUPS.

--- src/cups/genppd.c
+++ src/cups/genppd.c
@@ -1625,7 +1625,7 @@ write_ppd(
   stp_parameter_description_destroy(&desc);
 
   stp_describe_parameter(v, "OutputOrder", &desc);
-  if (desc.is_active && desc.p_type == STP_PARAMETER_TYPE_STRING_LIST)
+  if (desc.p_type == STP_PARAMETER_TYPE_STRING_LIST)
     {
       gzputs(fp, "*OpenUI *OutputOrder: PickOne\n");
       gzputs(fp, "*OrderDependency: 10 AnySetup *OutputOrder\n");
