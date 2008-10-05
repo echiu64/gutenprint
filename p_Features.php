396121dc68564cbbcaf98b867acce5945eb18118
Robert Krawitz <rlk@users.sourceforge.net>
2008-10-05 02:35:23 +0000

    .

--- p_Features.php
+++ p_Features.php
@@ -14,15 +14,15 @@ require('standard_html_header.php');
 <H2>Gutenprint Functionality</H2>
 
 <p>Gutenprint is a package of high quality printer drivers for Linux,
-BSD, Solaris, IRIX, and other UNIX-alike operating systems.  In many
-cases, these drivers rival or exceed the OEM drivers in quality and
-functionality.  Our goal is to produce the highest possible output
-quality from all supported printers.  To that end, we have done
-extensive work on screening algorithms, color generation, and printer
-feature utilization.  We are continuing our work in all of these areas
-to produce ever higher quality results, particularly on the
-ubiquitous, inexpensive inkjet printers that are nonetheless capable
-of nearly photographic output quality.</p>
+BSD, Solaris, Macintosh OS X, IRIX, and other UNIX-alike operating
+systems.  In many cases, these drivers rival or exceed the OEM drivers
+in quality and functionality.  Our goal is to produce the highest
+possible output quality from all supported printers.  To that end, we
+have done extensive work on screening algorithms, color generation,
+and printer feature utilization.  We are continuing our work in all of
+these areas to produce ever higher quality results, particularly on
+the ubiquitous, inexpensive inkjet printers that are nonetheless
+capable of nearly photographic output quality.</p>
 
 <p>Printing support for <a href="http://www.gimp.org">The GIMP</a> is
 only one small part of what Gutenprint does, and indeed, the Print
@@ -36,23 +36,17 @@ href="http://www.ghostscript.com">Ghostscript</a>.</p>
 
 <ul>
 
-<p><li>Support for almost 300 printers made by Canon, Epson,
-Hewlett-Packard, Lexmark, and true compatibles.</li></p>
+<p><li>Support for over 1300 inkjet, dye sublimation, and laser
+printers.</li></p>
 
-<p><li>The Print plug-in for the GIMP.</li></p>
+<p><li>An enhanced Print plug-in for the GIMP.</li></p>
 
 <p><li>A native CUPS driver, providing simple configuration and
 operation with CUPS.</li></p>
 
-<p><li>A native Ghostscript driver that can be compiled into
-Ghostscript.</li></p>
-
-<p><li>A new, IJS-based Ghostscript driver that greatly simplifies
-installation for Ghostscript versions 6.53 and newer.</li></p>
-
-<p><li>Complete <a
-href="http://www.linuxprinting.org/foomatic.html">Foomatic</a> support
-for simplified configuration with all common printing systems.</li></p>
+<p><li>An IJS-based Ghostscript driver that greatly simplifies
+installation for Ghostscript versions 6.53 and newer, when used with
+Foomatic.</li></p>
 
 <p><li>Extensive output adjustments offer a tremendous range of
 adjustment to match color, brightness, and contrast.</li></p>
