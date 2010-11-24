b2ff18642b91a1f716492a663f206c63e8d246a2
gernot2270 <>
2010-11-24 15:08:14 +0000

    added modes, media and printer definition for Canon MP480

--- src/main/canon-media.h
+++ src/main/canon-media.h
@@ -253,6 +253,24 @@ static const canon_paper_t canon_MULTIPASS_MP280_papers[] = {
 DECLARE_PAPERS(canon_MULTIPASS_MP280);
 
 /* Gernot: added ---- note: ESC ( P code not used at all yet, check print-canon.c */
+/* MP480 */
+static const canon_paper_t canon_MULTIPASS_MP480_papers[] = {
+  { "Plain",		N_ ("Plain Paper"),			0x00,0x00,0x00,1.00, 0.25, 0.500, 0, 0, 0 }, /* plain */
+  { "PhotoPlusGLoss2",  N_ ("Photo Paper Plus Glossy II"),	0x1d,0x23,0x32,0.78, 0.25, 0.500, 0, 0, 0 }, /* PPGgold */
+  { "PhotoPro2",	N_ ("Photo Paper Pro II"),	        0x1f,0x25,0x34,0.78, 0.25, 0.500, 0, 0, 0 }, /* PPGpro */
+  { "PhotoProPlat",	N_ ("Photo Paper Platinum"),	        0x1e,0x24,0x33,0.78, 0.25, 0.500, 0, 0, 0 }, /* PPGproPlat */
+  { "GlossyPhoto",	N_ ("Glossy Photo Paper"),		0x05,0x05,0x16,0.78, 0.25, 0.500, 0, 0, 0 }, /* PPG */
+  { "MattePhoto",	N_ ("Matte Photo Paper"),		0x0a,0x10,0x1c,0.78, 0.25, 0.500, 0, 0, 0 }, /* PP matte */
+  { "PhotopaperOther",	N_ ("Other Photo Paper"),		0x0f,0x14,0x24,0.78, 0.25, 0.500, 0, 0, 0 }, /* PP other */
+  { "Coated",		N_ ("High Resolution Paper"),		0x07,0x07,0x10,0.78, 0.25, 0.500, 0, 0, 0 }, /* hi res paper */
+  { "InkJetHagaki", 	N_ ("Ink Jet Hagaki"),			0x0d,0x09,0x1b,0.78, 0.25, 0.500, 0, 0, 0 }, /* inkjet hagaki */
+  { "Hagaki", 	        N_ ("Hagaki"),			        0x08,0x09,0x07,0.78, 0.25, 0.500, 0, 0, 0 }, /* hagaki*/
+  { "TShirt",		N_ ("T-Shirt Transfers"),		0x03,0x03,0x12,0.78, 0.25, 0.500, 0, 0, 0 }, /* T-shirt */
+  { "Envelope",		N_ ("Envelope"),			0x08,0x08,0x08,0.78, 0.25, 0.500, 0, 0, 0 }, /* env */
+};
+DECLARE_PAPERS(canon_MULTIPASS_MP480);
+
+/* Gernot: added ---- note: ESC ( P code not used at all yet, check print-canon.c */
 /* iP2700 series */
 static const canon_paper_t canon_PIXMA_iP2700_papers[] = {
   { "Plain",		N_ ("Plain Paper"),			0x00,0x00,0x00,1.00, 0.25, 0.500, 0, 0, 0 }, /* plain */
--- src/main/canon-printers.h
+++ src/main/canon-printers.h
@@ -886,6 +886,20 @@ static const canon_cap_t canon_model_capabilities[] =
     NULL,
     NULL
   },
+  { /* Canon MULTIPASS MP480 --- no XML */
+    "PIXMA MP480", 3,          /*model, model_id*/
+    842, 17*72,       /* max paper width and height */
+    10, 10, 15, 15,    /*border_left, border_right, border_top, border_bottom */
+    8,
+    &canon_MULTIPASS_MP250_slotlist, /* only rear tray also uses CAP_T */
+    CANON_CAP_STD0|CANON_CAP_DUPLEX|CANON_CAP_px|CANON_CAP_P|CANON_CAP_I,0,control_cmd_MULTIPASS_MP150,  /*features */
+    &canon_MULTIPASS_MP480_modelist,
+    &canon_MULTIPASS_MP480_paperlist,
+    NULL,
+    NULL,
+    NULL,
+    NULL
+  },
   { /* Canon MULTIPASS MP150 */
     "PIXMA MP150", 3,          /*model, model_id*/
     842, 17*72,       /* max paper width and height */
