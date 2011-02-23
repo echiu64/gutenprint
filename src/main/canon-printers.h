c18763fca1e0d7608ab39025b7ea8f86ead63c0c
gernot2270 <>
2011-02-23 15:06:14 +0000

    changed Canon MX870 control command set

--- src/main/canon-printers.h
+++ src/main/canon-printers.h
@@ -1202,7 +1202,7 @@ static const canon_cap_t canon_model_capabilities[] =
     10, 10, 15, 15,    /*border_left, border_right, border_top, border_bottom */
     8,
     &canon_PIXMA_MG5100_slotlist,
-    CANON_CAP_STD0|CANON_CAP_DUPLEX|CANON_CAP_r|CANON_CAP_px|CANON_CAP_P|CANON_CAP_I,0x64,control_cmd_PIXMA_iP4000, /*features*/
+    CANON_CAP_STD0|CANON_CAP_DUPLEX|CANON_CAP_r|CANON_CAP_px|CANON_CAP_P|CANON_CAP_I,0x64,control_cmd_MULTIPASS_MP150, /*features*/
     &canon_MULTIPASS_MX870_modelist,
     &canon_MULTIPASS_MX340_paperlist,
     NULL,
