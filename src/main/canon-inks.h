a202aa842a4f7b5d0ae398941c6c542862fa5e67
gernot2270 <>
2011-04-06 02:15:06 +0000

    add fast photo mode to Canon MP830 as test

--- src/main/canon-inks.h
+++ src/main/canon-inks.h
@@ -1416,7 +1416,32 @@ static const canon_inkset_t canon_22_C3M3Y3K2c3m3_c_inkset[] = {
         {'c',1.0,&canon_2b_3l_c_ink},
         {'m',1.0,&canon_2b_3l_c_ink},
         {0,0.0,NULL},
-	{'k',0.0,&canon_2b_4l_ink},  /* even though we won't use the photo black in this mode its parameters have to be set */
+	{'k',0.0,&canon_2b_3l_ink},  /* even though we won't use the photo black in this mode its parameters have to be set */
+        {0,0.0,NULL},
+        {0,0.0,NULL},
+        {0,0.0,NULL},
+        {0,0.0,NULL},
+        {0,0.0,NULL},
+        {0,0.0,NULL},
+        {0,0.0,NULL},
+        {0,0.0,NULL},
+        {0,0.0,NULL},
+        {0,0.0,NULL},
+        {0,0.0,NULL},
+        {0,0.0,NULL},
+        {0,0.0,NULL},
+        {0,0.0,NULL},
+};
+
+static const canon_inkset_t canon_22_C3M3Y3K2c3m3k3_c_inkset[] = {
+        {'C',1.0,&canon_2b_3l_c_ink},
+        {'M',1.0,&canon_2b_3l_c_ink},
+        {'Y',1.0,&canon_2b_3l_c_ink},
+        {'K',0.0,&canon_1b_2l_ink}, /* set to 0 */
+        {'c',1.0,&canon_2b_3l_c_ink},
+        {'m',1.0,&canon_2b_3l_c_ink},
+        {0,0.0,NULL},
+	{'k',1.0,&canon_2b_3l_c_ink},
         {0,0.0,NULL},
         {0,0.0,NULL},
         {0,0.0,NULL},
