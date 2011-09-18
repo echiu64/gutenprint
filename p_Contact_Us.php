9b036aa8bf176bbd5d2410281aad854195ba8a9d
Robert Krawitz <rlk@users.sourceforge.net>
2011-09-18 00:03:21 +0000

    Add contact info

---
+++ p_Contact_Us.php
@@ -0,0 +1,18 @@
+<?
+require('functions.php');
+###############################################
+##    Set title of this page here    ##########
+$title = 'Gutenprint Features';
+###############################################
+###############################################
+require('standard_html_header.php');
+
+
+### Content Below  ###
+# Please remember to use <P> </P> tags !  ?>
+
+<p>Please feel free to contact us at <a
+href="mailto:gimp-print-devel@lists.sourceforge.net">gimp-print-devel@lists.sourceforge.net</a>
+if you have any questions about Gutenprint.</p>
+
+<?require('standard_html_footer.php');?>
