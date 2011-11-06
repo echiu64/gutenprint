712bad65d83a02c1c858803fb30cef0fdd9ebc53
gernot2270 <>
2011-11-06 16:18:32 +0000

    Added modes to Canon iX5000. Corrected printer definition.

--- src/main/canon-printers.h
+++ src/main/canon-printers.h
@@ -1081,14 +1081,13 @@ static const canon_cap_t canon_model_capabilities[] =
     842, 17*72,       /* max paper width and height */
     10, 10, 15, 15,    /*border_left, border_right, border_top, border_bottom */
     &canon_MULTIPASS_MP170_slotlist,
-    /* CANON_CAP_STD0|CANON_CAP_DUPLEX|CANON_CAP_r|CANON_CAP_px|CANON_CAP_I|CANON_CAP_P,0x61,control_cmd_PIXMA_iP4000, */
-    CANON_CAP_STD0|CANON_CAP_px|CANON_CAP_I,0,control_cmd_PIXMA_iP4000,  /*features */
+    CANON_CAP_STD0|CANON_CAP_px|CANON_CAP_I|CANON_CAP_P,0,control_cmd_PIXMA_iP2700,  /*features */
     &canon_PIXMA_iX5000_modelist,
     &canon_PIXMA_iP1500_paperlist,
     NULL,
     NULL,
     NULL,
-    NULL
+    iP4500_channel_order
   },
   { /* Canon PIXMA iX6500 --- with XML */
     "PIXMA iX6500", 3,          /*model, model_id*/
