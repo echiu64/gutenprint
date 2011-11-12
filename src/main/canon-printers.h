06e074f967b926e97c7fdf11400c7555abdb8339
gernot2270 <>
2011-11-12 11:38:04 +0000

    Experimental support added for Canon iP8100

--- src/main/canon-printers.h
+++ src/main/canon-printers.h
@@ -1341,9 +1341,21 @@ static const canon_cap_t canon_model_capabilities[] =
     842, 17*72,       /* max paper width and height */
     10, 10, 15, 15,    /*border_left, border_right, border_top, border_bottom */
     &canon_PIXMA_iP4000_slotlist,
-    /*CANON_CAP_STD0|CANON_CAP_DUPLEX|CANON_CAP_r|CANON_CAP_px,0x61,control_cmd_PIXMA_iP4000,*/  /*features */
     CANON_CAP_STD0|CANON_CAP_DUPLEX|CANON_CAP_I|CANON_CAP_r|CANON_CAP_px|CANON_CAP_P,0x64,control_cmd_PIXMA_iP4200,  /*features */
     &canon_PIXMA_iP7100_modelist,
+    &canon_PIXMA_iP7100_paperlist,
+    NULL,
+    NULL,
+    NULL,
+    iP4500_channel_order
+  },
+  { /* Canon PIXMA iP8100 */
+    "PIXMA iP8100", 3,          /*model, model_id*/
+    842, 17*72,       /* max paper width and height */
+    10, 10, 15, 15,    /*border_left, border_right, border_top, border_bottom */
+    &canon_PIXMA_iP4000_slotlist,
+    CANON_CAP_STD0|CANON_CAP_DUPLEX|CANON_CAP_I|CANON_CAP_r|CANON_CAP_px|CANON_CAP_P,0x64,control_cmd_PIXMA_iP4200,  /*features */
+    &canon_PIXMA_iP8100_modelist,
     &canon_PIXMA_iP7100_paperlist,
     NULL,
     NULL,
