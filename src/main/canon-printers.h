8f3e750eaeebf018ca320fcd7a933ad140256cb9
gernot2270 <>
2011-11-13 16:28:59 +0000

    Added duplex capability, and Env/Hagaki modes to Canon MP640. Renamed and split out modes for easier readability.

--- src/main/canon-printers.h
+++ src/main/canon-printers.h
@@ -1263,13 +1263,13 @@ static const canon_cap_t canon_model_capabilities[] =
     842, 17*72,       /* max paper width and height */
     10, 10, 15, 15,    /*border_left, border_right, border_top, border_bottom */
     &canon_PIXMA_MG5200_slotlist,
-    CANON_CAP_STD0|CANON_CAP_px|CANON_CAP_I|CANON_CAP_XML|CANON_CAP_r|CANON_CAP_P,0x64,control_cmd_PIXMA_iP4200,  /*features */
+    CANON_CAP_STD0|CANON_CAP_px|CANON_CAP_DUPLEX|CANON_CAP_I|CANON_CAP_XML|CANON_CAP_r|CANON_CAP_P,0x64,control_cmd_PIXMA_iP4200,  /*features */
     &canon_MULTIPASS_MP640_modelist,
     &canon_MULTIPASS_MP640_paperlist,
     NULL,
     NULL,
     NULL,
-    NULL
+    iP4500_channel_order
   },
   { /* Canon PIXMA MP900 --- ESC (r is 2 byes: 0x62 0x0 but only 1 byte supported for now */
     "PIXMA MP900", 3,          /*model, model_id*/
