16d7d17b5cc15c95a203845d96e8709c50572849
gernot2270 <>
2011-11-15 16:54:58 +0000

    Added CAP_T capability to some Canon printers. Reorganized Canon iP4900 modes.

--- src/main/canon-printers.h
+++ src/main/canon-printers.h
@@ -1566,7 +1566,7 @@ static const canon_cap_t canon_model_capabilities[] =
     842, 17*72,       /* max paper width and height */
     10, 10, 15, 15,    /*border_left, border_right, border_top, border_bottom */
     &canon_MULTIPASS_MP250_slotlist, /* these models only have a rear tray. Also uses CAP_T  */
-    CANON_CAP_STD0|CANON_CAP_DUPLEX|CANON_CAP_px|CANON_CAP_P|CANON_CAP_I|CANON_CAP_XML,0,control_cmd_PIXMA_iP2700,  /*features */
+    CANON_CAP_STD0|CANON_CAP_DUPLEX|CANON_CAP_px|CANON_CAP_P|CANON_CAP_I|CANON_CAP_T|CANON_CAP_XML,0,control_cmd_PIXMA_iP2700,  /*features */
     &canon_MULTIPASS_MP250_modelist,
     &canon_MULTIPASS_MP250_paperlist,
     NULL,
