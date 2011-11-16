1dde0c995d0049b6addf2903cb0c6e00455f6a97
gernot2270 <>
2011-11-16 16:27:48 +0000

    Corrected Canon iP4800 command control set.

--- src/main/canon-printers.h
+++ src/main/canon-printers.h
@@ -1013,7 +1013,7 @@ static const canon_cap_t canon_model_capabilities[] =
     842, 17*72,       /* max paper width and height */
     10, 10, 15, 15,    /*border_left, border_right, border_top, border_bottom */
     &canon_PIXMA_iP4600_slotlist, /* there is some ESC (r 0x68 command for CD tray also, not sure why */
-    CANON_CAP_STD0|CANON_CAP_DUPLEX|CANON_CAP_r|CANON_CAP_px|CANON_CAP_I|CANON_CAP_P|CANON_CAP_XML,0x64,control_cmd_PIXMA_iP4200,  /* features */
+    CANON_CAP_STD0|CANON_CAP_DUPLEX|CANON_CAP_r|CANON_CAP_px|CANON_CAP_I|CANON_CAP_P|CANON_CAP_XML,0x64,control_cmd_PIXMA_iP2700,  /* features */
     &canon_PIXMA_iP4700_modelist,/* same for iP4800 */
     &canon_PIXMA_iP4700_paperlist,/* same for iP4800 */
     NULL,
