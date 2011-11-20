95dd39fd8868e6e3460673a758ba8974b3e8e851
gernot2270 <>
2011-11-20 16:17:48 +0000

    Added experimental support for Canon MP140 and changed driver for MP160 (clone of MP140).

--- src/main/canon-printers.h
+++ src/main/canon-printers.h
@@ -1748,6 +1748,19 @@ static const canon_cap_t canon_model_capabilities[] =
     NULL,
     iP4500_channel_order
   },
+  { /* Canon MULTIPASS MP140 */
+    "PIXMA MP140", 3,          /*model, model_id*/
+    842, 17*72,       /* max paper width and height */
+    10, 10, 15, 15,    /*border_left, border_right, border_top, border_bottom */
+    &canon_MULTIPASS_MP170_slotlist, /* these models only have an auto sheet feeder also uses CAP_T */
+    CANON_CAP_STD0|CANON_CAP_px|CANON_CAP_P|CANON_CAP_T|CANON_CAP_I,0,control_cmd_PIXMA_iP2700,  /*features */
+    &canon_MULTIPASS_MP150_modelist,
+    &canon_MULTIPASS_MP150_paperlist,
+    NULL,
+    NULL,
+    NULL,
+    iP4500_channel_order
+  },
   { /* Canon MULTIPASS MP150 */
     "PIXMA MP150", 3,          /*model, model_id*/
     842, 17*72,       /* max paper width and height */
