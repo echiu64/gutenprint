281eb165a2df9110350a3c9d919a7eb173bc5bb6
gernot2270 <>
2011-11-20 16:53:03 +0000

    Added experimental support for Canon MP210.

--- src/main/canon-printers.h
+++ src/main/canon-printers.h
@@ -1803,6 +1803,19 @@ static const canon_cap_t canon_model_capabilities[] =
     NULL,
     NULL
   },
+  { /* Canon MULTIPASS MP210 */
+    "PIXMA MP210", 3,          /*model, model_id*/
+    842, 17*72,       /* max paper width and height */
+    10, 10, 15, 15,    /*border_left, border_right, border_top, border_bottom */
+    &canon_MULTIPASS_MP250_slotlist, /* these models only have an auto sheet feeder also uses CAP_T */
+    CANON_CAP_STD0|CANON_CAP_px|CANON_CAP_P|CANON_CAP_T|CANON_CAP_I,0,control_cmd_PIXMA_iP2700,  /*features */
+    &canon_MULTIPASS_MP210_modelist,
+    &canon_MULTIPASS_MP150_paperlist,
+    NULL,
+    NULL,
+    NULL,
+    iP4500_channel_order
+  },
   { /* Canon MULTIPASS MP360/370/375R/390 */
     "PIXMA MP360", 3,          /*model, model_id*/
     842, 17*72,       /* max paper width and height */
