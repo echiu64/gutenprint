c33e6118a61d0abebcf865c4b29f23a347a5ce0a
gernot2270 <>
2011-11-21 09:22:22 +0000

    created separate printr definition for Canon iX4000.

--- src/main/canon-printers.h
+++ src/main/canon-printers.h
@@ -1449,6 +1449,19 @@ static const canon_cap_t canon_model_capabilities[] =
     NULL,
     iP4500_channel_order
   },
+  { /* Canon PIXMA iX4000 --- like iX5000 but includes Transparency and ud1 mode is different */
+    "PIXMA iX4000", 3,          /*model, model_id*/
+    842, 17*72,       /* max paper width and height */
+    10, 10, 15, 15,    /*border_left, border_right, border_top, border_bottom */
+    &canon_MULTIPASS_MP170_slotlist,
+    CANON_CAP_STD0|CANON_CAP_px|CANON_CAP_I|CANON_CAP_P,0,control_cmd_PIXMA_iP2700,  /*features */
+    &canon_PIXMA_iX4000_modelist,
+    &canon_PIXMA_iP1500_paperlist,
+    NULL,
+    NULL,
+    NULL,
+    iP4500_channel_order
+  },
   { /* Canon PIXMA iX5000 */
     "PIXMA iX5000", 3,          /*model, model_id*/
     842, 17*72,       /* max paper width and height */
