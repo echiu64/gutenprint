9ae1c4d82bead6f8ace72aa8ee36a8ea62573e9b
gernot2270 <>
2011-11-21 15:11:55 +0000

    Corrected Canon MP150 printer definition, customized modes.

--- src/main/canon-printers.h
+++ src/main/canon-printers.h
@@ -1791,17 +1791,14 @@ static const canon_cap_t canon_model_capabilities[] =
     "PIXMA MP150", 3,          /*model, model_id*/
     842, 17*72,       /* max paper width and height */
     10, 10, 15, 15,    /*border_left, border_right, border_top, border_bottom */
-    /* &canon_PIXMA_iP4000_slotlist, */
     &canon_MULTIPASS_MP170_slotlist, /* these models only have an auto sheet feeder also uses CAP_T */
-    /* CANON_CAP_STD0|CANON_CAP_DUPLEX|CANON_CAP_r|CANON_CAP_px|CANON_CAP_T|CANON_CAP_I,0x61,control_cmd_MULTIPASS_MP150, */
-    CANON_CAP_STD0|CANON_CAP_px|CANON_CAP_P|CANON_CAP_T|CANON_CAP_I,0,control_cmd_MULTIPASS_MP150,  /*features */
+    CANON_CAP_STD0|CANON_CAP_px|CANON_CAP_P|CANON_CAP_T|CANON_CAP_I,0,control_cmd_PIXMA_iP2700,  /*features */
     &canon_MULTIPASS_MP150_modelist,
-    /* &canon_PIXMA_iP4000_paperlist, */
     &canon_MULTIPASS_MP150_paperlist,
     NULL,
     NULL,
     NULL,
-    NULL
+    iP4500_channel_order
   },
   { /* Canon MULTIPASS MP170 */
     "PIXMA MP170", 3,          /*model, model_id*/
