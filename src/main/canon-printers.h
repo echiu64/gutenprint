17be6ac6a83914954f10aa90de500237f5d3cef5
gernot2270 <>
2011-11-24 13:42:30 +0000

    Corrected printer definition for Canon MX860, MX870, adjusted modes.

--- src/main/canon-printers.h
+++ src/main/canon-printers.h
@@ -2265,8 +2265,8 @@ static const canon_cap_t canon_model_capabilities[] =
     842, 17*72,       /* max paper width and height */
     10, 10, 15, 15,    /*border_left, border_right, border_top, border_bottom */
     &canon_PIXMA_MG5100_slotlist,
-    CANON_CAP_STD0|CANON_CAP_DUPLEX|CANON_CAP_r|CANON_CAP_px|CANON_CAP_P|CANON_CAP_I|CANON_CAP_XML,0x64,control_cmd_MULTIPASS_MP150, /*features*/
-    &canon_MULTIPASS_MX870_modelist,
+    CANON_CAP_STD0|CANON_CAP_DUPLEX|CANON_CAP_r|CANON_CAP_px|CANON_CAP_P|CANON_CAP_I|CANON_CAP_XML,0x64,control_cmd_PIXMA_iP2700, /*features*/
+    &canon_MULTIPASS_MX860_modelist,
     &canon_MULTIPASS_MX340_paperlist,
     NULL,
     NULL,
