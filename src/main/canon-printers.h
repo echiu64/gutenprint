59c7243e1831716dfae103ce05c85a7e3b56d79a
gernot2270 <>
2011-11-24 14:32:53 +0000

    Corrected printer definition for Canon MP493.

--- src/main/canon-printers.h
+++ src/main/canon-printers.h
@@ -1965,13 +1965,13 @@ static const canon_cap_t canon_model_capabilities[] =
     842, 17*72,       /* max paper width and height */
     10, 10, 15, 15,    /*border_left, border_right, border_top, border_bottom */
     &canon_MULTIPASS_MP250_slotlist, /* only rear tray also uses CAP_T */
-    CANON_CAP_STD0|CANON_CAP_px|CANON_CAP_P|CANON_CAP_T|CANON_CAP_I|CANON_CAP_XML,0,control_cmd_MULTIPASS_MP150,  /*features */
+    CANON_CAP_STD0|CANON_CAP_px|CANON_CAP_P|CANON_CAP_T|CANON_CAP_I|CANON_CAP_XML,0,control_cmd_PIXMA_iP2700,  /*features */
     &canon_MULTIPASS_MP480_modelist,
     &canon_MULTIPASS_MP493_paperlist,
     NULL,
     NULL,
     NULL,
-    NULL
+    iP4500_channel_order
   },
   { /* Canon MULTIPASS MP495 identical to MP280 it seems --- with XML */
     "PIXMA MP495", 3,          /*model, model_id*/
