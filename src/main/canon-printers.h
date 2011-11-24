da606aeefda8eb7c8880827a4c21d3844f1cebf3
gernot2270 <>
2011-11-24 13:23:42 +0000

    Corrected printer definition for Canon MX860, and corrected and enhanced modes. Added support for Env/Hagaki.

--- src/main/canon-printers.h
+++ src/main/canon-printers.h
@@ -2252,7 +2252,7 @@ static const canon_cap_t canon_model_capabilities[] =
     842, 17*72,       /* max paper width and height */
     10, 10, 15, 15,    /*border_left, border_right, border_top, border_bottom */
     &canon_PIXMA_MG5100_slotlist,
-    CANON_CAP_STD0|CANON_CAP_DUPLEX|CANON_CAP_r|CANON_CAP_px|CANON_CAP_P|CANON_CAP_I,0x64,control_cmd_PIXMA_iP4000, /*features*/
+    CANON_CAP_STD0|CANON_CAP_DUPLEX|CANON_CAP_r|CANON_CAP_px|CANON_CAP_P|CANON_CAP_I,0x64,control_cmd_PIXMA_iP4200, /*features*/
     &canon_MULTIPASS_MX860_modelist,
     &canon_MULTIPASS_MX330_paperlist,
     NULL,
