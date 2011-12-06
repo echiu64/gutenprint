bbf3ac81f32e37ea9e4f9017d1cb19b25fc031ca
rlk <>
2011-12-06 02:03:06 +0000

    Correctly initialize the default value, from Dmitry V. Levin.

--- src/main/print-escp2.c
+++ src/main/print-escp2.c
@@ -2168,6 +2168,8 @@ escp2_parameters(const stp_vars_t *v, const char *name,
   if (name == NULL)
     return;
 
+  memset(&description->deflt, 0, sizeof(description->deflt));
+
   for (i = 0; i < float_parameter_count; i++)
     if (strcmp(name, float_parameters[i].param.name) == 0)
       {
@@ -2202,7 +2204,6 @@ escp2_parameters(const stp_vars_t *v, const char *name,
 	break;
       }
 
-  description->deflt.str = NULL;
   if (strcmp(name, "AutoMode") == 0)
     {
       description->bounds.str = stp_string_list_create();
