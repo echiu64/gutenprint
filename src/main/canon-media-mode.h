dace027fae6979c8e57f819ee060033846763015
gernot2270 <>
2012-03-30 10:35:02 +0000

    Added Canon iP6000D media-mode data, corrected a photo mode name.

--- src/main/canon-media-mode.h
+++ src/main/canon-media-mode.h
@@ -151,6 +151,93 @@ static const canon_modeuse_t canon_PIXMA_iP4000_modeuses[] = {
 
 DECLARE_MODEUSES(canon_PIXMA_iP4000);
 
+/* ----------------------------------- Canon iP6000  ----------------------------------- */
+static const char* canon_PIXMA_iP6000_modeuses_plain[] = {
+  "600x600dpi_high2",
+  "600x600dpi_high",
+  "600x600dpi",
+  "300x300dpi",
+  "300x300dpi_draft",
+  NULL
+  };
+
+static const char* canon_PIXMA_iP6000_modeuses_PPpro[] = {
+  "600x600dpi_photohigh2",
+  "600x600dpi_photohigh",
+  "600x600dpi_photo",
+  NULL
+};
+
+static const char* canon_PIXMA_iP6000_modeuses_PPplus[] = {
+  "600x600dpi_photohigh",
+  "600x600dpi_photo",
+  NULL
+};
+
+static const char* canon_PIXMA_iP6000_modeuses_PPmatte[] = {
+  "600x600dpi_photohigh3",
+  "600x600dpi_photo",
+  NULL
+};
+
+/*Note: US printer driver does not support inkjetHagaki */
+static const char* canon_PIXMA_iP6000_modeuses_inkjetHagaki[] = {
+  "600x600dpi_photohigh2",
+  "600x600dpi_photohigh",
+  "600x600dpi_photo",
+  NULL
+};
+
+static const char* canon_PIXMA_iP6000_modeuses_Hagaki[] = {
+  "600x600dpi_high3",
+  "600x600dpi_std3",
+  "600x600dpi_draft3",
+  NULL
+};
+
+static const char* canon_PIXMA_iP6000_modeuses_disc[] = {
+  "600x600dpi_photohigh2",
+  "600x600dpi_photohigh",
+  "600x600dpi_photo",
+  NULL
+};
+
+static const char* canon_PIXMA_iP6000_modeuses_TShirt[] = {
+  "600x600dpi_tshirt",
+  NULL
+};
+
+static const char* canon_PIXMA_iP6000_modeuses_PPother[] = {
+  "600x600dpi_photohigh",
+  NULL
+  };
+
+static const char* canon_PIXMA_iP6000_modeuses_transparency[] = {
+  "600x600dpi_ohphigh",
+  "600x600dpi_ohp",
+  NULL
+};
+
+static const canon_modeuse_t canon_PIXMA_iP6000_modeuses[] = {
+ { "Plain",             canon_PIXMA_iP6000_modeuses_plain, 0 },
+ { "PhotopaperPro",	canon_PIXMA_iP6000_modeuses_PPpro, 0 },
+ { "PhotopaperPlus",	canon_PIXMA_iP6000_modeuses_PPplus, 0 },
+ { "PhotopaperPlusDouble", canon_PIXMA_iP6000_modeuses_PPplus, 0 },
+ { "PhotopaperMatte",	canon_PIXMA_iP6000_modeuses_PPmatte, 0 },
+ { "GlossyPaper",	canon_PIXMA_iP6000_modeuses_PPplus, 0 },
+ { "Coated",		canon_PIXMA_iP6000_modeuses_PPmatte, 0 },
+ { "InkJetHagaki", 	canon_PIXMA_iP6000_modeuses_inkjetHagaki, 0 },/*untested*/
+ { "Hagaki", 	        canon_PIXMA_iP6000_modeuses_Hagaki, 0 },
+ { "DiscCompat",	canon_PIXMA_iP6000_modeuses_disc, 0 },
+ { "DiscOthers",	canon_PIXMA_iP6000_modeuses_disc, 0 },
+ { "TShirt",		canon_PIXMA_iP6000_modeuses_TShirt, 0 },
+ { "Envelope",		canon_PIXMA_iP6000_modeuses_Hagaki, 0 },
+ { "PhotopaperOther",	canon_PIXMA_iP6000_modeuses_PPother, 0 },
+ { "Transparency",      canon_PIXMA_iP6000_modeuses_transparency, 0 },
+ };
+
+DECLARE_MODEUSES(canon_PIXMA_iP6000);
+
 /* ----------------------------------- Canon MP450  ----------------------------------- */
 static const char* canon_MULTIPASS_MP450_modeuses_plain[] = {
   "600x600dpi_high",
