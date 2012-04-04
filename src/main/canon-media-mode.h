2a01663bc82ccdd7c2cfdbe8a255d710af8dc89a
gernot2270 <>
2012-04-04 12:57:47 +0000

    Added Canon iP4900 to media-mode combinations.

--- src/main/canon-media-mode.h
+++ src/main/canon-media-mode.h
@@ -563,6 +563,84 @@ static const canon_modeuse_t canon_PIXMA_iP4700_modeuses[] = {
 
 DECLARE_MODEUSES(canon_PIXMA_iP4700);
 
+/* ----------------------------------- Canon iP4900  ----------------------------------- */
+static const char* canon_PIXMA_iP4900_modeuses_plain[] = {
+  "600x600dpi_high",
+  "600x600dpi_high2",/* duplex */
+  "600x600dpi",
+  "600x600dpi_std2",
+  "300x300dpi",
+  NULL
+  };
+
+static const char* canon_PIXMA_iP4900_modeuses_PPplusG2[] = {
+  "600x600dpi_photohigh",
+  "600x600dpi_photo",
+  NULL
+};
+
+/* highest mode not yet supported */
+static const char* canon_PIXMA_iP4900_modeuses_PPproPlat[] = {
+  "600x600dpi_photohigh",
+  "600x600dpi_photo",
+  NULL
+};
+
+static const char* canon_PIXMA_iP4900_modeuses_PPmatte[] = {
+  "600x600dpi_photohigh",
+  "600x600dpi_photo2",
+  NULL
+};
+
+static const char* canon_PIXMA_iP4900_modeuses_inkjetHagaki[] = {
+  "600x600dpi_photohigh4",
+  "600x600dpi_photo4",
+  NULL
+};
+
+static const char* canon_PIXMA_iP4900_modeuses_Hagaki[] = {
+  "600x600dpi_high2",
+  "600x600dpi_std2",
+  NULL
+};
+
+static const char* canon_PIXMA_iP4900_modeuses_disc[] = {
+  "600x600dpi_photohigh3",
+  "600x600dpi_photo3",
+  NULL
+};
+
+static const char* canon_PIXMA_iP4900_modeuses_TShirt[] = {
+  "600x600dpi_tshirt",
+  NULL
+};
+
+static const char* canon_PIXMA_iP4900_modeuses_PPother[] = {
+  "600x600dpi_photo",
+  NULL
+  };
+
+static const canon_modeuse_t canon_PIXMA_iP4900_modeuses[] = {
+  { "Plain",            canon_PIXMA_iP4900_modeuses_plain, DUPLEX_MODEREPL },
+  { "PhotoPlusGloss2",	canon_PIXMA_iP4900_modeuses_PPplusG2, 0 },
+  { "PhotoProPlat",	canon_PIXMA_iP4900_modeuses_PPproPlat, 0 },
+  { "PhotoProSemiGloss",canon_PIXMA_iP4900_modeuses_PPplusG2, 0 },
+  { "GlossyPaper",	canon_PIXMA_iP4900_modeuses_PPplusG2, 0 },
+  { "PhotopaperMatte",	canon_PIXMA_iP4900_modeuses_PPmatte, 0 },
+  { "Coated",	        canon_PIXMA_iP4900_modeuses_PPmatte, 0 },
+  { "HagakiA", 	        canon_PIXMA_iP4900_modeuses_Hagaki, 0 },
+  { "InkJetHagaki", 	canon_PIXMA_iP4900_modeuses_inkjetHagaki, 0 },
+  { "CanonPhotoHagakiK",canon_PIXMA_iP4900_modeuses_PPplusG2, 0 },
+  { "Hagaki", 	        canon_PIXMA_iP4900_modeuses_Hagaki, 0 },
+  { "DiscCompat",	canon_PIXMA_iP4900_modeuses_disc, 0 },
+  { "DiscOthers",	canon_PIXMA_iP4900_modeuses_disc, 0 },
+  { "TShirt",	        canon_PIXMA_iP4900_modeuses_TShirt, 0 },
+  { "Envelope",		canon_PIXMA_iP4900_modeuses_Hagaki, 0 },
+  { "PhotopaperOther",	canon_PIXMA_iP4900_modeuses_PPother, 0 },
+};
+
+DECLARE_MODEUSES(canon_PIXMA_iP4900);
+
 /* ----------------------------------- Canon iP5300  ----------------------------------- */
 static const char* canon_PIXMA_iP5300_modeuses_plain[] = {
   "600x600dpi_high",
