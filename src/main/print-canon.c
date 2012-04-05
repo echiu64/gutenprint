41e6e25d123cc82c945b7decdd60ddca451664f7
gernot2270 <>
2012-04-05 04:34:36 +0000

    Added Canon iX4000 to mode-media data.

--- src/main/print-canon.c
+++ src/main/print-canon.c
@@ -501,6 +501,7 @@ static const canon_mode_t* canon_get_current_mode(const stp_vars_t *v){
           const canon_modeuselist_t* mlist = &canon_PIXMA_iP2000_modeuselist;
           const canon_modeuselist_t* mlist = &canon_PIXMA_iP2700_modeuselist;
 	  const canon_modeuselist_t* mlist = &canon_PIXMA_iP3000_modeuselist;
+	  const canon_modeuselist_t* mlist = &canon_PIXMA_iP3100_modeuselist;
 	  const canon_modeuselist_t* mlist = &canon_PIXMA_iP4000_modeuselist;
 	  const canon_modeuselist_t* mlist = &canon_PIXMA_iP4100_modeuselist;
 	  const canon_modeuselist_t* mlist = &canon_PIXMA_iP4200_modeuselist;
@@ -526,7 +527,7 @@ static const canon_mode_t* canon_get_current_mode(const stp_vars_t *v){
 	  const canon_modeuselist_t* mlist = &canon_MULTIPASS_MX360_modeuselist;
 	  const canon_modeuselist_t* mlist = &canon_PIXMA_Pro9000_modeuselist;
 	  const canon_modeuselist_t* mlist = &canon_PIXMA_Pro9000mk2_modeuselist;*/
-    const canon_modeuselist_t* mlist = &canon_PIXMA_iP3100_modeuselist;
+	  const canon_modeuselist_t* mlist = &canon_PIXMA_iX4000_modeuselist;
 
     const canon_modeuse_t* muse = NULL;
     const canon_paper_t* media_type = get_media_type(caps,stp_get_string_parameter(v, "MediaType"));
@@ -583,6 +584,7 @@ static const canon_mode_t* canon_get_current_mode(const stp_vars_t *v){
       if ( (!strcmp(caps->name,"PIXMA iP2000")) ) {
       if ( (!strcmp(caps->name,"PIXMA iP2700")) ) {
       if ( (!strcmp(caps->name,"PIXMA iP3000")) ) {
+      if ( (!strcmp(caps->name,"PIXMA iP3100")) ) {
       if ( (!strcmp(caps->name,"PIXMA iP4000")) ) {
       if ( (!strcmp(caps->name,"PIXMA iP4100")) ) {
       if ( (!strcmp(caps->name,"PIXMA iP4200")) ) {
@@ -608,7 +610,7 @@ static const canon_mode_t* canon_get_current_mode(const stp_vars_t *v){
       if ( (!strcmp(caps->name,"PIXMA MX360")) ) {
       if ( (!strcmp(caps->name,"PIXMA Pro9000")) ) {
       if ( (!strcmp(caps->name,"PIXMA Pro9002")) ) {*/
-      if ( (!strcmp(caps->name,"PIXMA iP3100")) ) {
+      if ( (!strcmp(caps->name,"PIXMA iX4000")) ) {
 
 	
 	stp_dprintf(STP_DBG_CANON, v,"DEBUG: Gutenprint: media type selected: '%s'\n",media_type->name);
@@ -3785,6 +3787,7 @@ canon_do_print(stp_vars_t *v, stp_image_t *image)
       const canon_modeuselist_t* mlist = &canon_PIXMA_iP2000_modeuselist;
       const canon_modeuselist_t* mlist = &canon_PIXMA_iP2700_modeuselist;
       const canon_modeuselist_t* mlist = &canon_PIXMA_iP3000_modeuselist;
+      const canon_modeuselist_t* mlist = &canon_PIXMA_iP3100_modeuselist;
       const canon_modeuselist_t* mlist = &canon_PIXMA_iP4000_modeuselist;
       const canon_modeuselist_t* mlist = &canon_PIXMA_iP4100_modeuselist;
       const canon_modeuselist_t* mlist = &canon_PIXMA_iP4200_modeuselist;
@@ -3810,7 +3813,7 @@ canon_do_print(stp_vars_t *v, stp_image_t *image)
       const canon_modeuselist_t* mlist = &canon_MULTIPASS_MX360_modeuselist;
       const canon_modeuselist_t* mlist = &canon_PIXMA_Pro9000_modeuselist;
       const canon_modeuselist_t* mlist = &canon_PIXMA_Pro9000mk2_modeuselist;*/
-      const canon_modeuselist_t* mlist = &canon_PIXMA_iP3100_modeuselist;
+      const canon_modeuselist_t* mlist = &canon_PIXMA_iX4000_modeuselist;
 
   
   const canon_modeuse_t* muse = NULL;
@@ -3880,6 +3883,7 @@ canon_do_print(stp_vars_t *v, stp_image_t *image)
       if ( (!strcmp(caps->name,"PIXMA iP2000")) ) {
       if ( (!strcmp(caps->name,"PIXMA iP2700")) ) {
       if ( (!strcmp(caps->name,"PIXMA iP3000")) ) {
+      if ( (!strcmp(caps->name,"PIXMA iP3100")) ) {
       if ( (!strcmp(caps->name,"PIXMA iP4000")) ) {
       if ( (!strcmp(caps->name,"PIXMA iP4100")) ) {
       if ( (!strcmp(caps->name,"PIXMA iP4200")) ) {
@@ -3905,7 +3909,7 @@ canon_do_print(stp_vars_t *v, stp_image_t *image)
       if ( (!strcmp(caps->name,"PIXMA MX360")) ) {
       if ( (!strcmp(caps->name,"PIXMA Pro9000")) ) {
       if ( (!strcmp(caps->name,"PIXMA Pro9002")) ) {*/
-      if ( (!strcmp(caps->name,"PIXMA iP3100")) ) {
+      if ( (!strcmp(caps->name,"PIXMA iX4000")) ) {
     
     /* scroll through modeuse list to find media */
     for(i=0;i<mlist->count;i++){
