6f2f6ca5c40439de20e093a12210682095fd2b09
gernot2270 <>
2012-04-09 07:39:06 +0000

    Added Canon MX340 (and MX350) to mode-media data.

--- src/main/print-canon.c
+++ src/main/print-canon.c
@@ -560,11 +560,12 @@ static const canon_mode_t* canon_get_current_mode(const stp_vars_t *v){
 	  const canon_modeuselist_t* mlist = &canon_MULTIPASS_MP970_modeuselist;
 	  const canon_modeuselist_t* mlist = &canon_MULTIPASS_MP990_modeuselist;
 	  const canon_modeuselist_t* mlist = &canon_MULTIPASS_MX300_modeuselist;
+	  const canon_modeuselist_t* mlist = &canon_MULTIPASS_MX330_modeuselist;
 	  const canon_modeuselist_t* mlist = &canon_MULTIPASS_MX360_modeuselist;
 	  const canon_modeuselist_t* mlist = &canon_MULTIPASS_MX420_modeuselist;
 	  const canon_modeuselist_t* mlist = &canon_PIXMA_Pro9000_modeuselist;
 	  const canon_modeuselist_t* mlist = &canon_PIXMA_Pro9000mk2_modeuselist;*/
-	  const canon_modeuselist_t* mlist = &canon_MULTIPASS_MX330_modeuselist;
+	  const canon_modeuselist_t* mlist = &canon_MULTIPASS_MX340_modeuselist;
 
     const canon_modeuse_t* muse = NULL;
     const canon_paper_t* media_type = get_media_type(caps,stp_get_string_parameter(v, "MediaType"));
@@ -680,11 +681,12 @@ static const canon_mode_t* canon_get_current_mode(const stp_vars_t *v){
       if ( (!strcmp(caps->name,"PIXMA MP970")) ) {
       if ( (!strcmp(caps->name,"PIXMA MP990")) ) {
       if ( (!strcmp(caps->name,"PIXMA MX300")) ) {
+      if ( (!strcmp(caps->name,"PIXMA MX330")) ) {
       if ( (!strcmp(caps->name,"PIXMA MX360")) ) {
       if ( (!strcmp(caps->name,"PIXMA MX420")) ) {
       if ( (!strcmp(caps->name,"PIXMA Pro9000")) ) {
       if ( (!strcmp(caps->name,"PIXMA Pro9002")) ) {*/
-      if ( (!strcmp(caps->name,"PIXMA MX330")) ) {
+      if ( (!strcmp(caps->name,"PIXMA MX340")) ) {
 	
 	stp_dprintf(STP_DBG_CANON, v,"DEBUG: Gutenprint: media type selected: '%s'\n",media_type->name);
 	if (ERRPRINT)
@@ -3919,11 +3921,12 @@ canon_do_print(stp_vars_t *v, stp_image_t *image)
       const canon_modeuselist_t* mlist = &canon_MULTIPASS_MP970_modeuselist;
       const canon_modeuselist_t* mlist = &canon_MULTIPASS_MP990_modeuselist;
       const canon_modeuselist_t* mlist = &canon_MULTIPASS_MX300_modeuselist;
+      const canon_modeuselist_t* mlist = &canon_MULTIPASS_MX330_modeuselist;
       const canon_modeuselist_t* mlist = &canon_MULTIPASS_MX360_modeuselist;
       const canon_modeuselist_t* mlist = &canon_MULTIPASS_MX420_modeuselist;
       const canon_modeuselist_t* mlist = &canon_PIXMA_Pro9000_modeuselist;
       const canon_modeuselist_t* mlist = &canon_PIXMA_Pro9000mk2_modeuselist;*/
-      const canon_modeuselist_t* mlist = &canon_MULTIPASS_MX330_modeuselist;
+      const canon_modeuselist_t* mlist = &canon_MULTIPASS_MX340_modeuselist;
 
   
   const canon_modeuse_t* muse = NULL;
@@ -4052,11 +4055,12 @@ canon_do_print(stp_vars_t *v, stp_image_t *image)
       if ( (!strcmp(caps->name,"PIXMA MP970")) ) {
       if ( (!strcmp(caps->name,"PIXMA MP990")) ) {
       if ( (!strcmp(caps->name,"PIXMA MX300")) ) {
+      if ( (!strcmp(caps->name,"PIXMA MX330")) ) {
       if ( (!strcmp(caps->name,"PIXMA MX360")) ) {
       if ( (!strcmp(caps->name,"PIXMA MX420")) ) {
       if ( (!strcmp(caps->name,"PIXMA Pro9000")) ) {
       if ( (!strcmp(caps->name,"PIXMA Pro9002")) ) {*/
-      if ( (!strcmp(caps->name,"PIXMA MX330")) ) {
+      if ( (!strcmp(caps->name,"PIXMA MX340")) ) {
     
     /* scroll through modeuse list to find media */
     for(i=0;i<mlist->count;i++){
