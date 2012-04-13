f35f001fd01a58197cc9cbd96b6e6fbfbea62b37
gernot2270 <>
2012-04-13 17:01:52 +0000

    Added Canon MG8100 to media-mode combinations.

--- src/main/print-canon.c
+++ src/main/print-canon.c
@@ -594,11 +594,12 @@ static const canon_mode_t* canon_get_current_mode(const stp_vars_t *v){
 	  const canon_modeuselist_t* mlist = &canon_MULTIPASS_MX7600_modeuselist;
 	  const canon_modeuselist_t* mlist = &canon_PIXMA_MG5100_modeuselist;
 	  const canon_modeuselist_t* mlist = &canon_PIXMA_MG5200_modeuselist;
+	  const canon_modeuselist_t* mlist = &canon_PIXMA_MG6100_modeuselist;
 	  const canon_modeuselist_t* mlist = &canon_PIXMA_Pro9000_modeuselist;
 	  const canon_modeuselist_t* mlist = &canon_PIXMA_Pro9000mk2_modeuselist;
 	  const canon_modeuselist_t* mlist = &canon_PIXMA_Pro9500_modeuselist;
 	  const canon_modeuselist_t* mlist = &canon_PIXMA_Pro9500mk2_modeuselist;*/
-	  const canon_modeuselist_t* mlist = &canon_PIXMA_MG6100_modeuselist;
+	  const canon_modeuselist_t* mlist = &canon_PIXMA_MG8100_modeuselist;
 
 
     const canon_modeuse_t* muse = NULL;
@@ -749,11 +750,12 @@ static const canon_mode_t* canon_get_current_mode(const stp_vars_t *v){
       if ( (!strcmp(caps->name,"PIXMA MX7600")) ) {
       if ( (!strcmp(caps->name,"PIXMA MG5100")) ) {
       if ( (!strcmp(caps->name,"PIXMA MG5200")) ) {
+      if ( (!strcmp(caps->name,"PIXMA MG6100")) ) {
       if ( (!strcmp(caps->name,"PIXMA Pro9000")) ) {
       if ( (!strcmp(caps->name,"PIXMA Pro9002")) ) {
       if ( (!strcmp(caps->name,"PIXMA Pro9500")) ) {
       if ( (!strcmp(caps->name,"PIXMA Pro9502")) ) {*/
-      if ( (!strcmp(caps->name,"PIXMA MG6100")) ) {
+      if ( (!strcmp(caps->name,"PIXMA MG8100")) ) {
 	
 	stp_dprintf(STP_DBG_CANON, v,"DEBUG: Gutenprint: media type selected: '%s'\n",media_type->name);
 	if (ERRPRINT)
@@ -4022,11 +4024,12 @@ canon_do_print(stp_vars_t *v, stp_image_t *image)
       const canon_modeuselist_t* mlist = &canon_MULTIPASS_MX7600_modeuselist;
       const canon_modeuselist_t* mlist = &canon_PIXMA_MG5100_modeuselist;
       const canon_modeuselist_t* mlist = &canon_PIXMA_MG5200_modeuselist;
+      const canon_modeuselist_t* mlist = &canon_PIXMA_MG6100_modeuselist;
       const canon_modeuselist_t* mlist = &canon_PIXMA_Pro9000_modeuselist;
       const canon_modeuselist_t* mlist = &canon_PIXMA_Pro9000mk2_modeuselist;
       const canon_modeuselist_t* mlist = &canon_PIXMA_Pro9500_modeuselist;
       const canon_modeuselist_t* mlist = &canon_PIXMA_Pro9500mk2_modeuselist;*/
-      const canon_modeuselist_t* mlist = &canon_PIXMA_MG6100_modeuselist;
+      const canon_modeuselist_t* mlist = &canon_PIXMA_MG8100_modeuselist;
 
   
   const canon_modeuse_t* muse = NULL;
@@ -4189,11 +4192,12 @@ canon_do_print(stp_vars_t *v, stp_image_t *image)
       if ( (!strcmp(caps->name,"PIXMA MX7600")) ) {
       if ( (!strcmp(caps->name,"PIXMA MG5100")) ) {
       if ( (!strcmp(caps->name,"PIXMA MG5200")) ) {
+      if ( (!strcmp(caps->name,"PIXMA MG6100")) ) {
       if ( (!strcmp(caps->name,"PIXMA Pro9000")) ) {
       if ( (!strcmp(caps->name,"PIXMA Pro9002")) ) {
       if ( (!strcmp(caps->name,"PIXMA Pro9500")) ) {
       if ( (!strcmp(caps->name,"PIXMA Pro9502")) ) {*/
-      if ( (!strcmp(caps->name,"PIXMA MG6100")) ) {
+      if ( (!strcmp(caps->name,"PIXMA MG8100")) ) {
     
     /* scroll through modeuse list to find media */
     for(i=0;i<mlist->count;i++){
