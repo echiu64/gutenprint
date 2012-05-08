598557e438727e0e4af113b6a510c0d8fa6f1999
gernot2270 <>
2012-05-08 16:41:36 +0000

    Added partial media-mode data (less flags) for Canon E500.

--- src/main/canon-media-mode.h
+++ src/main/canon-media-mode.h
@@ -9235,6 +9235,65 @@ static const canon_modeuse_t canon_MULTIPASS_MX7600_modeuses[] = {
 
 DECLARE_MODEUSES(canon_MULTIPASS_MX7600);
 
+/* ----------------------------------- Canon E500  ----------------------------------- */
+static const char* canon_MULTIPASS_E500_modeuses_plain[] = {
+  "600x600dpi_high",
+  "600x600dpi_high2",/* color-only */
+  "600x600dpi",
+  "600x600dpi_draft",/*untested*/
+  "300x300dpi",
+  "300x300dpi_draft",
+  NULL
+};
+
+static const char* canon_MULTIPASS_E500_modeuses_PPplusG2[] = {
+  "600x600dpi_photohigh",
+  "600x600dpi_photo",
+  NULL
+};
+
+static const char* canon_MULTIPASS_E500_modeuses_PPproPlat[] = {
+  "600x600dpi_photohigh2",
+  "600x600dpi_photohigh",
+  "600x600dpi_photo",
+  NULL
+};
+
+static const char* canon_MULTIPASS_E500_modeuses_Hagaki[] = {
+  "600x600dpi_high3",
+  "600x600dpi_std3",
+  NULL
+};
+
+static const char* canon_MULTIPASS_E500_modeuses_TShirt[] = {
+  "600x600dpi_tshirt",
+  NULL
+};
+
+static const char* canon_MULTIPASS_E500_modeuses_PPother[] = {
+  "600x600dpi_photohigh",
+  NULL
+};
+
+static const canon_modeuse_t canon_MULTIPASS_E500_modeuses[] = {
+  { "Plain",            canon_MULTIPASS_E500_modeuses_plain, 0 },
+  { "PhotoPlusGloss2",  canon_MULTIPASS_E500_modeuses_PPplusG2, 0 },
+  { "PhotoProPlat",     canon_MULTIPASS_E500_modeuses_PPproPlat, 0 },
+  { "PhotoProSemiGloss",canon_MULTIPASS_E500_modeuses_PPplusG2, 0 },
+  { "GlossyPaper",	canon_MULTIPASS_E500_modeuses_PPplusG2, 0 },
+  { "PhotopaperMatte",	canon_MULTIPASS_E500_modeuses_PPplusG2, 0 },
+  { "Coated",		canon_MULTIPASS_E500_modeuses_PPplusG2, 0 },
+  { "HagakiA", 	        canon_MULTIPASS_E500_modeuses_Hagaki, 0 },
+  { "InkJetHagaki", 	canon_MULTIPASS_E500_modeuses_PPplusG2, 0 },
+  { "CanonPhotoHagakiK",canon_MULTIPASS_E500_modeuses_PPplusG2, 0 },
+  { "Hagaki", 	        canon_MULTIPASS_E500_modeuses_Hagaki, 0 },
+  { "TShirt",		canon_MULTIPASS_E500_modeuses_TShirt, 0 },
+  { "Envelope",		canon_MULTIPASS_E500_modeuses_Hagaki, 0 },
+  { "PhotopaperOther",	canon_MULTIPASS_E500_modeuses_PPother, 0 },
+};
+
+DECLARE_MODEUSES(canon_MULTIPASS_E500);
+
 /* ----------------------------------- Canon MG2100  ----------------------------------- */
 static const char* canon_PIXMA_MG2100_modeuses_plain[] = {
   "600x600dpi_high",
