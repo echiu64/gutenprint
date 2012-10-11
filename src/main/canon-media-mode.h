afc17d6950ac491449542d674f9fe32e568d7814
gernot2270 <>
2012-10-11 15:10:46 +0000

    Added support for Canon MG2200 series.

--- src/main/canon-media-mode.h
+++ src/main/canon-media-mode.h
@@ -9441,6 +9441,83 @@ static const canon_modeuse_t canon_PIXMA_MG2100_modeuses[] = {
 
 DECLARE_MODEUSES(canon_PIXMA_MG2100);
 
+/* ----------------------------------- Canon MG2200  ----------------------------------- */
+static const char* canon_PIXMA_MG2200_modeuses_plain[] = {
+  "600x600dpi_high",
+  "600x600dpi",
+  "300x300dpi",
+  /* Mono */
+  "600x600dpi_monohigh",
+  "600x600dpi_mono",
+  "300x300dpi_mono",
+  /* Color */
+  "600x600dpi_high2",
+  "600x600dpi_std2",
+  "300x300dpi_std2",
+  NULL
+  };
+
+static const char* canon_PIXMA_MG2200_modeuses_PPplusG2[] = {
+  "600x600dpi_photohigh",
+  "600x600dpi_photo",
+  NULL
+};
+
+static const char* canon_PIXMA_MG2200_modeuses_PPpro[] = {
+  "600x600dpi_photohigh2",
+  "600x600dpi_photohigh",
+  "600x600dpi_photo",
+  NULL
+};
+
+static const char* canon_PIXMA_MG2200_modeuses_PPproLuster[] = {
+  "600x600dpi_photohigh",
+  "600x600dpi_photo",
+  NULL
+};
+
+static const char* canon_PIXMA_MG2200_modeuses_Hagaki[] = {
+  "600x600dpi_high3",
+  "600x600dpi_std3",
+  /* Mono */
+  "600x600dpi_highmono3",
+  "600x600dpi_mono3",
+  /* Color */
+  "600x600dpi_high4",
+  "600x600dpi_std4",
+  NULL
+};
+
+static const char* canon_PIXMA_MG2200_modeuses_TShirt[] = {
+  "600x600dpi_tshirt",
+  NULL
+};
+
+static const char* canon_PIXMA_MG2200_modeuses_PPother[] = {
+  "600x600dpi_photohigh",
+  NULL
+};
+
+static const canon_modeuse_t canon_PIXMA_MG2200_modeuses[] = {
+  { "Plain",            canon_PIXMA_MG2200_modeuses_plain, INKSET_BLACK_SUPPORT | INKSET_COLOR_SUPPORT | INKSET_BLACK_MODEREPL | INKSET_COLOR_MODEREPL },
+  { "PhotoPlusGloss2",  canon_PIXMA_MG2200_modeuses_PPplusG2, INKSET_COLOR_SUPPORT },
+  { "PhotoProPlat",     canon_PIXMA_MG2200_modeuses_PPpro, INKSET_COLOR_SUPPORT },
+  { "PhotoProLuster",   canon_PIXMA_MG2200_modeuses_PPproLuster, INKSET_COLOR_SUPPORT },
+  { "PhotoProSemiGloss",canon_PIXMA_MG2200_modeuses_PPplusG2, INKSET_COLOR_SUPPORT },
+  { "PhotopaperMatte",	canon_PIXMA_MG2200_modeuses_PPplusG2, INKSET_COLOR_SUPPORT },
+  { "GlossyPaper",	canon_PIXMA_MG2200_modeuses_PPplusG2, INKSET_COLOR_SUPPORT },
+  { "Coated",		canon_PIXMA_MG2200_modeuses_PPplusG2, INKSET_COLOR_SUPPORT },
+  { "HagakiA", 	        canon_PIXMA_MG2200_modeuses_Hagaki, INKSET_BLACK_SUPPORT | INKSET_COLOR_SUPPORT | INKSET_BLACK_MODEREPL | INKSET_COLOR_MODEREPL },
+  { "InkJetHagaki", 	canon_PIXMA_MG2200_modeuses_PPplusG2, INKSET_COLOR_SUPPORT },
+  { "CanonPhotoHagakiK",canon_PIXMA_MG2200_modeuses_PPplusG2, INKSET_COLOR_SUPPORT },
+  { "Hagaki", 	        canon_PIXMA_MG2200_modeuses_Hagaki, INKSET_BLACK_SUPPORT | INKSET_COLOR_SUPPORT | INKSET_BLACK_MODEREPL | INKSET_COLOR_MODEREPL },
+  { "TShirt",		canon_PIXMA_MG2200_modeuses_TShirt, INKSET_COLOR_SUPPORT },
+  { "Envelope",		canon_PIXMA_MG2200_modeuses_Hagaki, INKSET_BLACK_SUPPORT | INKSET_COLOR_SUPPORT | INKSET_BLACK_MODEREPL | INKSET_COLOR_MODEREPL }, 
+  { "PhotopaperOther",	canon_PIXMA_MG2200_modeuses_PPother, INKSET_COLOR_SUPPORT },
+};
+
+DECLARE_MODEUSES(canon_PIXMA_MG2200);
+
 /* ----------------------------------- Canon MG3100  ----------------------------------- */
 static const char* canon_PIXMA_MG3100_modeuses_plain[] = {
   "600x600dpi_high",
--- src/main/canon-printers.h
+++ src/main/canon-printers.h
@@ -2524,6 +2524,22 @@ static const canon_cap_t canon_model_capabilities[] =
     NULL,
     iP4500_channel_order
   },
+  { /* Canon PIXMA MG2200 */
+    "PIXMA MG2200", 3,          /*model, model_id*/
+    INCH(17/2), 1917,       /* max paper width and height */ /* no linux driver yet to check this */
+    10, 10, 9, 15,    /*border_left, border_right, border_top, border_bottom */
+    &canon_PIXMA_MG2100_slotlist,
+    CANON_CAP_STD0|CANON_CAP_px|CANON_CAP_I|CANON_CAP_P|CANON_CAP_T|CANON_CAP_XML|CANON_CAP_BORDERLESS,0,
+    3,6, /* ESC (l and (P command lengths */
+    control_cmd_PIXMA_MG5300,  /* features Also uses CAP_T */
+    &canon_PIXMA_MG2100_modelist,
+    &canon_PIXMA_MG3200_paperlist,
+    &canon_PIXMA_MG2200_modeuselist,
+    NULL,
+    NULL,
+    NULL,
+    iP4500_channel_order
+  },
   { /* Canon PIXMA MG3100 */
     "PIXMA MG3100", 3,          /*model, model_id*/
     INCH(17/2), 1917,       /* max paper width and height */ /* from linux driver v3.60 */
@@ -2542,7 +2558,7 @@ static const canon_cap_t canon_model_capabilities[] =
   },
   { /* Canon PIXMA MG3200 */
     "PIXMA MG3200", 3,          /*model, model_id*/
-    INCH(17/2), 1917,       /* max paper width and height */ /* from linux driver v3.60 --- ?  */
+    INCH(17/2), 1917,       /* max paper width and height */ /* no linux driver yet to check this */
     10, 10, 9, 15,    /*border_left, border_right, border_top, border_bottom */
     &canon_PIXMA_MG2100_slotlist,
     CANON_CAP_STD0|CANON_CAP_DUPLEX|CANON_CAP_px|CANON_CAP_I|CANON_CAP_P|CANON_CAP_T|CANON_CAP_XML|CANON_CAP_BORDERLESS,0,
