14e298f771e1eaa5cbbd371538e4d6ee6cc80646
speachy <>
2012-11-18 17:11:00 +0000

    [selphy]  Initial commit of the Canon Dyesub (Selphy CP/ES) CUPS backend.

---
+++ src/cups/selphy_print.c
@@ -0,0 +1,543 @@
+/*
+ *   Canon SELPHY ES/CP series print assister -- libusb-1.0 version
+ *
+ *   (c) 2007-2012 Solomon Peachy <pizza@shaftnet.org>
+ *   
+ *   This program is free software; you can redistribute it and/or modify it
+ *   under the terms of the GNU General Public License as published by the Free
+ *   Software Foundation; either version 2 of the License, or (at your option)
+ *   any later version.
+ *
+ *   This program is distributed in the hope that it will be useful, but
+ *   WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ *   or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ *   for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program; if not, write to the Free Software
+ *   Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ *
+ *          [http://www.gnu.org/licenses/gpl-3.0.html]
+ *
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+#include <libusb-1.0/libusb.h>
+
+#include "selphy_print_common.h"
+
+#define STR_LEN_MAX 64
+#define URI_PREFIX "selphy://"
+
+/* USB Identifiers */
+#define USB_VID_CANON       0x04a9
+#define USB_PID_CANON_ES1   0x3141
+#define USB_PID_CANON_ES2   0x3185
+#define USB_PID_CANON_ES20  0x3186
+#define USB_PID_CANON_ES3   3   // XXX 31af? 31b1??
+#define USB_PID_CANON_ES30  0x31B0
+#define USB_PID_CANON_ES40  0x31EE
+#define USB_PID_CANON_CP10  0x304A
+#define USB_PID_CANON_CP100 0x3063 // - incoming G
+#define USB_PID_CANON_CP200 200 // XXX 307c? 307e? - incoming S (related to cp300)
+#define USB_PID_CANON_CP220 0x30BD
+#define USB_PID_CANON_CP300 0x307D
+#define USB_PID_CANON_CP330 0x30BE
+#define USB_PID_CANON_CP400 0x30F6
+#define USB_PID_CANON_CP500 500 // XXX 30f5? 30f7? (related to cp400?)
+#define USB_PID_CANON_CP510 0x3128
+#define USB_PID_CANON_CP520 520 // XXX 316f? 3172? (related to cp740/cp750)
+#define USB_PID_CANON_CP530 530 // XXX
+#define USB_PID_CANON_CP600 0x310B
+#define USB_PID_CANON_CP710 0x3127
+#define USB_PID_CANON_CP720 0x3143
+#define USB_PID_CANON_CP730 0x3142
+#define USB_PID_CANON_CP740 0x3171
+#define USB_PID_CANON_CP750 0x3170
+#define USB_PID_CANON_CP760 0x31AB
+#define USB_PID_CANON_CP770 0x31AA
+#define USB_PID_CANON_CP780 0x31DD
+#define USB_PID_CANON_CP790 790 // XXX
+#define USB_PID_CANON_CP800 0x3214
+#define USB_PID_CANON_CP810 0x3256
+#define USB_PID_CANON_CP900 0x3255
+
+static int dump_data_libusb(int remaining, int present, int data_fd, 
+			    struct libusb_device_handle *dev, 
+			    uint8_t endpoint,
+			    uint8_t *buf, uint16_t buflen) {
+	int cnt;
+	int i;
+	int wrote;
+	int num;
+
+	while (remaining > 0) {
+		cnt = read(data_fd, buf + present, (remaining < (buflen-present)) ? remaining : (buflen-present));
+		
+		if (cnt < 0)
+			return -1;
+
+		if (present) {
+			cnt += present;
+			present = 0;
+		}
+
+		i = libusb_bulk_transfer(dev, endpoint,
+					 buf,
+					 cnt,
+					 &num,
+					 2000);
+		if (i < 0) {
+			ERROR("libusb error %d: (%d/%d to 0x%02x)\n", i, num, cnt, endpoint);
+			return -1;
+		}
+
+		if (num != cnt) {
+			/* Realign buffer.. */
+			present = cnt - num;
+			memmove(buf, buf + num, present);
+		}
+		wrote += num;
+		remaining -= num;
+	}
+	
+	DEBUG("Wrote %d bytes\n", wrote);
+	
+	return wrote;
+}
+
+static int find_and_enumerate(struct libusb_context *ctx,
+			      struct libusb_device ***list,
+			      int printer_type,
+			      int scan_only)
+{
+	int num;
+	int i;
+	int found = -1;
+
+	struct libusb_device_handle *dev;
+
+	/* Enumerate and find suitable device */
+	num = libusb_get_device_list(ctx, list);
+
+	for (i = 0 ; i < num ; i++) {
+		struct libusb_device_descriptor desc;
+		unsigned char product[STR_LEN_MAX] = "";
+		unsigned char serial[STR_LEN_MAX] = "";
+		unsigned char manuf[STR_LEN_MAX] = "";
+		int valid = 0;
+		libusb_get_device_descriptor((*list)[i], &desc);
+
+		if (desc.idVendor != USB_VID_CANON)
+			continue;
+
+		switch(desc.idProduct) {
+		case USB_PID_CANON_ES1: // "Canon SELPHY ES1"
+			if (printer_type == P_ES1)
+				found = i;
+			valid = 1;
+			break;
+		case USB_PID_CANON_ES2: // "Canon SELPHY ES2"
+		case USB_PID_CANON_ES20: // "Canon SELPHY ES20"
+			if (printer_type == P_ES2_20)
+				found = i;
+			valid = 1;
+			break;
+		case USB_PID_CANON_ES3: // "Canon SELPHY ES3"
+		case USB_PID_CANON_ES30: // "Canon SELPHY ES30"
+			if (printer_type == P_ES3_30)
+				found = i;
+			valid = 1;
+			break;
+		case USB_PID_CANON_ES40: // "Canon SELPHY ES40"
+		case USB_PID_CANON_CP790:
+			if (printer_type == P_ES40_CP790)
+				found = i;
+			valid = 1;
+			break;
+		case USB_PID_CANON_CP10: // "Canon CP-10"
+		case USB_PID_CANON_CP100: // "Canon CP-100"
+		case USB_PID_CANON_CP200: // "Canon CP-200"
+		case USB_PID_CANON_CP220: // "Canon CP-220"
+		case USB_PID_CANON_CP300: // "Canon CP-300"
+		case USB_PID_CANON_CP330: // "Canon CP-330"
+		case USB_PID_CANON_CP400: // "Canon SELPHY CP400"
+		case USB_PID_CANON_CP500: // "Canon SELPHY CP500"
+		case USB_PID_CANON_CP510: // "Canon SELPHY CP510"
+		case USB_PID_CANON_CP520: // "Canon SELPHY CP520"
+		case USB_PID_CANON_CP530: // "Canon SELPHY CP530"
+		case USB_PID_CANON_CP600: // "Canon SELPHY CP600"
+		case USB_PID_CANON_CP710: // "Canon SELPHY CP710"
+		case USB_PID_CANON_CP720: // "Canon SELPHY CP720"
+		case USB_PID_CANON_CP730: // "Canon SELPHY CP730"
+		case USB_PID_CANON_CP740: // "Canon SELPHY CP740"
+		case USB_PID_CANON_CP750: // "Canon SELPHY CP750"
+		case USB_PID_CANON_CP760: // "Canon SELPHY CP760"
+		case USB_PID_CANON_CP770: // "Canon SELPHY CP770"
+		case USB_PID_CANON_CP780: // "Canon SELPHY CP780"
+		case USB_PID_CANON_CP800: // "Canon SELPHY CP800"
+		case USB_PID_CANON_CP810: // "Canon SELPHY CP810"
+			if (printer_type == P_CP_XXX)
+				found = i;
+			valid = 1;
+			break;
+		case USB_PID_CANON_CP900: // "Canon SELPHY CP900"
+			/* XXX deliberate.  no way to distinguish P_CP900 based
+			   on a streamed-in print job */
+			if (printer_type == P_CP_XXX)
+				found = i;
+			valid = 1;
+			break;
+		default:
+			break;
+		}
+
+		if (libusb_open(((*list)[i]), &dev)) {
+			ERROR("Could not open device %04x:%04x\n", desc.idVendor, desc.idProduct);
+			found = -1;
+			continue;
+		}
+
+		/* Query detailed info */
+		if (desc.iManufacturer) {
+			libusb_get_string_descriptor_ascii(dev, desc.iManufacturer, manuf, STR_LEN_MAX);
+		}
+		if (desc.iProduct) {
+			libusb_get_string_descriptor_ascii(dev, desc.iProduct, product, STR_LEN_MAX);
+		}
+		if (desc.iSerialNumber) {
+			libusb_get_string_descriptor_ascii(dev, desc.iSerialNumber, serial, STR_LEN_MAX);
+		}
+
+		DEBUG("%s%sPID: %04X Product: '%s' Serial: '%s'\n",
+		      (!valid) ? "UNRECOGNIZED: " : "",
+		      (found == i) ? "MATCH: " : "",
+		      desc.idProduct, product, serial);
+		
+		// XXX MATCH based on passed-in serial number?
+
+		if (valid && scan_only) {
+			/* URL-ify model. */
+			char buf[128]; // XXX ugly..
+			i = 0;
+			while (*(product + i + strlen("Canon"))) {
+			  buf[i] = *(product + i + strlen("Canon "));
+			  if(buf[i] == ' ') {
+			    buf[i++] = '%';
+			    buf[i++] = '2';
+			    buf[i] = '0';
+			  }
+			  i++;
+			}
+			fprintf(stdout, "direct %sCanon/%s?serial=%s \"%s\" \"%s\" \"MFG:Canon;CMD:SelphyRaster;CLS:PRINTER;MDL:%s;DES:%s;SN:%s\" \"\"\n", URI_PREFIX,
+			        buf, serial, product, product,
+				product + strlen("Canon "), product, serial);
+		}
+
+		libusb_close(dev);
+	}
+
+	return found;
+}
+
+int main (int argc, char **argv)
+{
+	struct libusb_context *ctx;
+	struct libusb_device **list;
+	struct libusb_device_handle *dev;
+	struct libusb_config_descriptor *config;
+
+	uint8_t endp_up = 0;
+	uint8_t endp_down = 0;
+
+	int printer_type = P_END;
+
+	int iface = 0;
+
+	int num, i;
+	int ret = 0;
+	int claimed;
+	int found = -1;
+
+	uint8_t rdbuf[READBACK_LEN], rdbuf2[READBACK_LEN];
+	int last_state = -1, state = S_IDLE;
+
+	int plane_len = 0;
+
+	int bw_mode = 0;
+	int16_t paper_code_offset = -1;
+	int16_t paper_code = -1;
+
+	uint8_t buffer[BUF_LEN];
+
+	int data_fd = fileno(stdin);
+
+	char *uri = getenv("DEVICE_URI");;
+	char *use_serno = NULL;
+
+	/* Static initialization */
+	setup_paper_codes();
+
+	/* Cmdline help */
+	if (argc < 2) {
+		DEBUG("SELPHY ES/CP Print Assist version %s\nUsage:\n\t%s [ infile | - ]\n\t%s job user title num-copies options [ filename ] \n\n",
+		      VERSION,
+		      argv[0], argv[0]);
+		libusb_init(&ctx);
+		find_and_enumerate(ctx, &list, printer_type, 1);
+		libusb_free_device_list(list, 1);
+		libusb_exit(ctx);
+		exit(1);
+	}
+
+	/* Are we running as a CUPS backend? */
+	if (uri) {
+		if (argv[6]) {  /* IOW, is it specified? */
+			data_fd = open(argv[6], O_RDONLY);
+			if (data_fd < 0) {
+				perror("ERROR:Can't open input file");
+				exit(1);
+			}
+		}
+
+		/* Start parsing URI 'selphy://PID/SERIAL' */
+		if (strncmp(URI_PREFIX, uri, strlen(URI_PREFIX))) {
+			ERROR("Invalid URI prefix (%s)\n", uri);
+			exit(1);
+		}
+		use_serno = strchr(uri, '=');
+		if (!use_serno || !*(use_serno+1)) {
+			ERROR("Invalid URI (%s)\n", uri);
+			exit(1);
+		}
+		use_serno++;
+	} else {
+		/* Open Input File */
+		if (strcmp("-", argv[1])) {
+			data_fd = open(argv[1], O_RDONLY);
+			if (data_fd < 0) {
+				perror("ERROR:Can't open input file");
+				exit(1);
+			}
+		}
+	}
+
+	/* Figure out printer this file is intended for */
+	read(data_fd, buffer, MAX_HEADER);
+
+	printer_type = parse_printjob(buffer, &bw_mode, &plane_len);
+	if (printer_type < 0) {
+		ERROR("Unrecognized printjob file format!\n");
+		exit(1);
+	}
+
+	DEBUG("%sFile intended for a '%s' printer\n",  bw_mode? "B/W " : "", printers[printer_type].model);
+
+	plane_len += 12; /* Add in plane header */
+	paper_code_offset = printers[printer_type].paper_code_offset;
+	if (printers[printer_type].pgcode_offset != -1)
+		paper_code = printers[printer_type].paper_codes[buffer[printers[printer_type].pgcode_offset]];
+
+	/* Libusb setup */
+	libusb_init(&ctx);
+	found = find_and_enumerate(ctx, &list, printer_type, 0);
+
+	if (found == -1) {
+		ERROR("No suitable printers found!\n");
+		ret = 3;
+		goto done;
+	}
+
+	ret = libusb_open(list[found], &dev);
+	if (ret) {
+		ERROR("Could not open device (Need to be root?) (%d)\n", ret);
+		ret = 4;
+		goto done;
+	}
+	
+	claimed = libusb_kernel_driver_active(dev, iface);
+	if (claimed) {
+		ret = libusb_detach_kernel_driver(dev, iface);
+		if (ret) {
+			ERROR("Could not detach printer from kernel (%d)\n", ret);
+			ret = 4;
+			goto done_close;
+		}
+	}
+
+	ret = libusb_claim_interface(dev, iface);
+	if (ret) {
+		ERROR("Could not claim printer interface (%d)\n", ret);
+		ret = 4;
+		goto done_close;
+	}
+
+	ret = libusb_get_active_config_descriptor(list[found], &config);
+	if (ret) {
+		ERROR("Could not fetch config descriptor (%d)\n", ret);
+		ret = 4;
+		goto done_close;
+	}
+
+	for (i = 0 ; i < config->interface[0].altsetting[0].bNumEndpoints ; i++) {
+		if ((config->interface[0].altsetting[0].endpoint[i].bmAttributes & 3) == LIBUSB_TRANSFER_TYPE_BULK) {
+			if (config->interface[0].altsetting[0].endpoint[i].bEndpointAddress & LIBUSB_ENDPOINT_IN)
+				endp_up = config->interface[0].altsetting[0].endpoint[i].bEndpointAddress;
+			else
+				endp_down = config->interface[0].altsetting[0].endpoint[i].bEndpointAddress;				
+		}
+	}
+
+top:
+	/* Read in the printer status */
+	ret = libusb_bulk_transfer(dev, endp_up,
+				   rdbuf,
+				   READBACK_LEN,
+				   &num,
+				   2000);
+	if (ret < 0) {
+		ERROR("libusb error %d: (%d/%d from 0x%02x)\n", ret, num, READBACK_LEN, endp_up);
+		ret = 4;
+		goto done_claimed;
+	}
+
+	if (memcmp(rdbuf, rdbuf2, READBACK_LEN)) {
+		DEBUG("readback:  %02x %02x %02x %02x  %02x %02x %02x %02x  %02x %02x %02x %02x\n",
+			rdbuf[0], rdbuf[1], rdbuf[2], rdbuf[3],
+			rdbuf[4], rdbuf[5], rdbuf[6], rdbuf[7],
+			rdbuf[8], rdbuf[9], rdbuf[10], rdbuf[11]);
+		memcpy(rdbuf2, rdbuf, READBACK_LEN);
+	} else {
+		sleep(1);
+	}
+	if (state != last_state) {
+		DEBUG("last_state %d new %d\n", last_state, state);
+		last_state = state;
+	}
+	fflush(stderr);       
+
+	switch(state) {
+	case S_IDLE:
+		if (!fancy_memcmp(rdbuf, printers[printer_type].init_readback, READBACK_LEN, paper_code_offset, paper_code)) {
+			state = S_PRINTER_READY;
+		}
+		break;
+	case S_PRINTER_READY:
+		DEBUG("Sending init sequence (%d bytes)\n", printers[printer_type].init_length);
+
+		/* Send printer init */
+		ret = libusb_bulk_transfer(dev, endp_down,
+					   buffer,
+					   printers[printer_type].init_length,
+					   &num,
+					   2000);
+		if (ret < 0) {
+			ERROR("libusb error %d: (%d/%d to 0x%02x)\n", ret, num, printers[printer_type].init_length, endp_down);
+			ret = 4;
+			goto done_claimed;
+		}
+
+		/* Realign plane data to start of buffer.. */
+		memmove(buffer, buffer+printers[printer_type].init_length,
+			MAX_HEADER-printers[printer_type].init_length);
+
+		state = S_PRINTER_INIT_SENT;
+		break;
+	case S_PRINTER_INIT_SENT:
+		if (!fancy_memcmp(rdbuf, printers[printer_type].ready_y_readback, READBACK_LEN, paper_code_offset, paper_code)) {
+			state = S_PRINTER_READY_Y;
+		}
+		break;
+	case S_PRINTER_READY_Y:
+		if (bw_mode)
+			DEBUG("Sending BLACK plane\n");
+		else
+			DEBUG("Sending YELLOW plane\n");
+		ret = dump_data_libusb(plane_len, MAX_HEADER-printers[printer_type].init_length, data_fd, dev, endp_down, buffer, BUF_LEN);
+		if (ret < 0) {
+			ret = 4;
+			goto done_claimed;
+		}
+		state = S_PRINTER_Y_SENT;
+		break;
+	case S_PRINTER_Y_SENT:
+		if (!fancy_memcmp(rdbuf, printers[printer_type].ready_m_readback, READBACK_LEN, paper_code_offset, paper_code)) {
+			if (bw_mode)
+				state = S_PRINTER_DONE;
+			else
+				state = S_PRINTER_READY_M;
+		}
+		break;
+	case S_PRINTER_READY_M:
+		DEBUG("Sending MAGENTA plane\n");
+		ret = dump_data_libusb(plane_len, 0, data_fd, dev, endp_down, buffer, BUF_LEN);
+		if (ret < 0) {
+			ret = 4;
+			goto done_claimed;
+		}
+		state = S_PRINTER_M_SENT;
+		break;
+	case S_PRINTER_M_SENT:
+		if (!fancy_memcmp(rdbuf, printers[printer_type].ready_c_readback, READBACK_LEN, paper_code_offset, paper_code)) {
+			state = S_PRINTER_READY_C;
+		}
+		break;
+	case S_PRINTER_READY_C:
+		DEBUG("Sending CYAN plane\n");
+		ret = dump_data_libusb(plane_len, 0, data_fd, dev, endp_down, buffer, BUF_LEN);
+		if (ret < 0) {
+			ret = 4;
+			goto done_claimed;
+		}
+		state = S_PRINTER_C_SENT;
+		break;
+	case S_PRINTER_C_SENT:
+		if (!fancy_memcmp(rdbuf, printers[printer_type].done_c_readback, READBACK_LEN, paper_code_offset, paper_code)) {
+			state = S_PRINTER_DONE;
+		}
+		break;
+	case S_PRINTER_DONE:
+		if (printers[printer_type].foot_length) {
+			DEBUG("Sending cleanup sequence\n");
+			ret = dump_data_libusb(printers[printer_type].foot_length, 0, data_fd, dev, endp_down, buffer, BUF_LEN);
+			if (ret < 0) {
+				ret = 4;
+				goto done_claimed;
+			}
+		}
+		state = S_FINISHED;
+		/* Intentional Fallthrough */
+	case S_FINISHED:
+		DEBUG("All data sent to printer!\n");	
+		break;
+	}
+	if (state != S_FINISHED)
+		goto top;
+
+	/* Done printing */
+
+done_claimed:
+	libusb_release_interface(dev, iface);
+
+done_close:
+	if (claimed)
+		libusb_attach_kernel_driver(dev, iface);
+
+	libusb_close(dev);
+
+done:
+	libusb_free_device_list(list, 1);
+	libusb_exit(ctx);
+
+	close(data_fd);
+
+	return ret;
+}
---
+++ src/cups/selphy_print_common.h
@@ -0,0 +1,289 @@
+/*
+ *   Canon SELPHY ES/CP series print assister -- Common Code
+ *
+ *   (c) 2007-2012 Solomon Peachy <pizza@shaftnet.org>
+ *  
+ *   This program is free software; you can redistribute it and/or modify it
+ *   under the terms of the GNU General Public License as published by the Free
+ *   Software Foundation; either version 2 of the License, or (at your option)
+ *   any later version.
+ *
+ *   This program is distributed in the hope that it will be useful, but
+ *   WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ *   or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ *   for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program; if not, write to the Free Software
+ *   Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ *
+ *          [http://www.gnu.org/licenses/gpl-3.0.html]
+ *
+ */
+
+#define VERSION "0.30"
+
+#define DEBUG( ... ) fprintf(stderr, "DEBUG: " __VA_ARGS__ )
+#define ERROR( ... ) fprintf(stderr, "ERROR: " __VA_ARGS__ )
+
+#if (__BYTE_ORDER == __LITTLE_ENDIAN)
+#define le32_to_cpu(__x) __x
+#else
+#define le32_to_cpu(x)							\
+	({								\
+		uint32_t __x = (x);					\
+		((uint32_t)(						\
+			(((uint32_t)(__x) & (uint32_t)0x000000ffUL) << 24) | \
+			(((uint32_t)(__x) & (uint32_t)0x0000ff00UL) <<  8) | \
+			(((uint32_t)(__x) & (uint32_t)0x00ff0000UL) >>  8) | \
+			(((uint32_t)(__x) & (uint32_t)0xff000000UL) >> 24) )); \
+	})
+#endif
+
+#define READBACK_LEN 12
+
+struct printer_data {
+	int  type;  /* P_??? */
+	char *model; /* eg "SELPHY ES1" */
+	int  init_length;
+	int  foot_length;
+	int16_t init_readback[READBACK_LEN];
+	int16_t ready_y_readback[READBACK_LEN];
+	int16_t ready_m_readback[READBACK_LEN];
+	int16_t ready_c_readback[READBACK_LEN];
+	int16_t done_c_readback[READBACK_LEN];
+	int16_t paper_codes[256];
+	int16_t pgcode_offset;  /* Offset into printjob for paper type */
+	int16_t paper_code_offset; /* Offset in readback for paper type */
+};
+
+/* printer types */
+enum {
+	P_ES1 = 0,
+	P_ES2_20,
+	P_ES3_30,
+	P_ES40_CP790,
+	P_CP900,
+	P_CP_XXX,
+	P_END
+};
+
+struct printer_data printers[P_END] = {
+	{ .type = P_ES1,
+	  .model = "SELPHY ES1",
+	  .init_length = 12,
+	  .foot_length = 0,
+	  .init_readback = { 0x02, 0x00, 0x00, 0x00, 0x02, 0x01, -1, 0x01, 0x00, 0x00, 0x00, 0x00 },
+	  .ready_y_readback = { 0x04, 0x00, 0x01, 0x00, 0x02, 0x01, -1, 0x01, 0x00, 0x00, 0x00, 0x00 },
+	  .ready_m_readback = { 0x04, 0x00, 0x03, 0x00, 0x02, 0x01, -1, 0x01, 0x00, 0x00, 0x00, 0x00 },
+	  .ready_c_readback = { 0x04, 0x00, 0x07, 0x00, 0x02, 0x01, -1, 0x01, 0x00, 0x00, 0x00, 0x00 },
+	  .done_c_readback = { 0x04, 0x00, 0x00, 0x00, 0x02, 0x01, -1, 0x01, 0x00, 0x00, 0x00, 0x00 },
+	  // .paper_codes
+	  .pgcode_offset = 3,
+	  .paper_code_offset = 6,
+	},
+	{ .type = P_ES2_20,
+	  .model = "SELPHY ES2/ES20",
+	  .init_length = 16,
+	  .foot_length = 0,
+	  .init_readback = { 0x02, 0x00, 0x00, 0x00, -1, 0x00, -1, -1, 0x00, 0x00, 0x00, 0x00 },
+	  .ready_y_readback = { 0x03, 0x00, 0x01, 0x00, -1, 0x00, -1, -1, 0x00, 0x00, 0x00, 0x00 },
+	  .ready_m_readback = { 0x06, 0x00, 0x03, 0x00, -1, 0x00, -1, -1, 0x00, 0x00, 0x00, 0x00 },
+	  .ready_c_readback = { 0x09, 0x00, 0x07, 0x00, -1, 0x00, -1, -1, 0x00, 0x00, 0x00, 0x00 },
+	  .done_c_readback = { 0x09, 0x00, 0x00, 0x00, -1, 0x00, -1, -1, 0x00, 0x00, 0x00, 0x00 },
+	  // .paper_codes
+	  .pgcode_offset = 2,
+	  .paper_code_offset = 4,
+	},
+	{ .type = P_ES3_30,
+	  .model = "SELPHY ES3/ES30",
+	  .init_length = 16,
+	  .foot_length = 12,
+	  .init_readback = { 0x00, 0xff, 0x00, 0x00, 0xff, 0xff, 0xff, 0xff, 0x00, 0x00, 0x00, 0x00 },
+	  .ready_y_readback = { 0x01, 0xff, 0x01, 0x00, 0xff, 0xff, 0xff, 0xff, 0x00, 0x00, 0x00, 0x00 },
+	  .ready_m_readback = { 0x03, 0xff, 0x02, 0x00, 0xff, 0xff, 0xff, 0xff, 0x00, 0x00, 0x00, 0x00 },
+	  .ready_c_readback = { 0x05, 0xff, 0x03, 0x00, 0xff, 0xff, 0xff, 0xff, 0x00, 0x00, 0x00, 0x00 },
+	  .done_c_readback = { 0x00, 0xff, 0x10, 0x00, 0xff, 0xff, 0xff, 0xff, 0x00, 0x00, 0x00, 0x00 },
+	  // .paper_codes
+	  .pgcode_offset = -1,
+	  .paper_code_offset = -1,
+	},
+	{ .type = P_ES40_CP790,
+	  .model = "SELPHY ES40/CP790",
+	  .init_length = 16,
+	  .foot_length = 12,
+//	  .init_readback = 
+//	  .ready_y_readback = 
+//	  .ready_m_readback = 
+//	  .ready_c_readback = 
+//	  .done_c_readback = 
+	  // .paper_codes
+	  .pgcode_offset = 2,
+//	  .paper_code_offset = -1,
+	},
+	{ .type = P_CP900,  // XXX readbacks and paper_code_offset is a GUESS
+	  .model = "SELPHY CP900",
+	  .init_length = 12,
+	  .foot_length = 4,
+	  .init_readback = { 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, -1, 0x00, 0x00, 0x00, 0x00, -1 },
+	  .ready_y_readback = { 0x02, 0x00, 0x00, 0x00, 0x70, 0x00, -1, 0x00, 0x00, 0x00, 0x00, -1 },
+	  .ready_m_readback = { 0x04, 0x00, 0x00, 0x00, 0x00, 0x00, -1, 0x00, 0x00, 0x00, 0x00, -1 },
+	  .ready_c_readback = { 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, -1, 0x00, 0x00, 0x00, 0x00, -1 },
+	  .done_c_readback = { 0x20, 0x00, 0x00, 0x00, 0x00, 0x00, -1, 0x00, 0x00, 0x00, 0x00, -1 },
+	  // .paper_codes
+	  .pgcode_offset = 3,
+	  .paper_code_offset = 6,
+	},
+	{ .type = P_CP_XXX,
+	  .model = "SELPHY CP Series (!CP790/CP900)",
+	  .init_length = 12,
+	  .foot_length = 0,
+	  .init_readback = { 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, -1, 0x00, 0x00, 0x00, 0x00, -1 },
+	  .ready_y_readback = { 0x02, 0x00, 0x00, 0x00, 0x70, 0x00, -1, 0x00, 0x00, 0x00, 0x00, -1 },
+	  .ready_m_readback = { 0x04, 0x00, 0x00, 0x00, 0x00, 0x00, -1, 0x00, 0x00, 0x00, 0x00, -1 },
+	  .ready_c_readback = { 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, -1, 0x00, 0x00, 0x00, 0x00, -1 },
+	  .done_c_readback = { 0x20, 0x00, 0x00, 0x00, 0x00, 0x00, -1, 0x00, 0x00, 0x00, 0x00, -1 },
+	  // .paper_codes
+	  .pgcode_offset = 3,
+	  .paper_code_offset = 6,
+	},
+};
+
+#define MAX_HEADER 28
+#define BUF_LEN 4096
+
+static const int es40_cp790_plane_lengths[4] = { 2227456, 1601600, 698880, 2976512 };
+
+static void setup_paper_codes(void)
+{
+	/* Default all to IGNORE */
+	int i, j;
+	for (i = 0; i < P_END ; i++)
+		for (j = 0 ; j < 256 ; j++) 
+			printers[i].paper_codes[j] = -1;
+	
+	/* SELPHY ES1 paper codes */
+	printers[P_ES1].paper_codes[0x11] = 0x01;
+	printers[P_ES1].paper_codes[0x12] = 0x02; // ? guess
+	printers[P_ES1].paper_codes[0x13] = 0x03;
+	
+	/* SELPHY ES2/20 paper codes */
+	printers[P_ES2_20].paper_codes[0x01] = 0x01;
+	printers[P_ES2_20].paper_codes[0x02] = 0x02; // ? guess
+	printers[P_ES2_20].paper_codes[0x03] = 0x03;
+	
+	/* SELPHY ES3/30 paper codes -- N/A, printer does not report paper type */
+	//  printers[P_ES3_30]paper_codes[0x01] = -1;
+	//  printers[P_ES3_30]paper_codes[0x02] = -1;
+	//  printers[P_ES3_30]paper_codes[0x03] = -1;
+	
+	/* SELPHY ES40/CP790 paper codes -- UNKNOWN */
+	//  printers[P_ES40_CP790].paper_codes[0x00] = -1;
+	//  printers[P_ES40_CP790].paper_codes[0x01] = -1;
+	//  printers[P_ES40_CP790].paper_codes[0x02] = -1;
+	//  printers[P_ES40_CP790].paper_codes[0x03] = -1;
+
+	/* SELPHY CP-900 paper codes */
+	printers[P_CP900].paper_codes[0x01] = 0x11; // ? guess
+	printers[P_CP900].paper_codes[0x02] = 0x22; // ? guess
+	printers[P_CP900].paper_codes[0x03] = 0x33; // ? guess
+
+	/* SELPHY CP-series (except CP790/CP900) paper codes */
+	printers[P_CP_XXX].paper_codes[0x01] = 0x11;
+	printers[P_CP_XXX].paper_codes[0x02] = 0x22;
+	printers[P_CP_XXX].paper_codes[0x03] = 0x33;
+	printers[P_CP_XXX].paper_codes[0x04] = 0x44;
+}
+
+#define INCORRECT_PAPER -999
+
+static int fancy_memcmp(const uint8_t *buf_a, const int16_t *buf_b, uint len, int16_t papercode_offset, int16_t papercode_val) 
+{
+	int i;
+  
+	for (i = 0 ; i < len ; i++) {
+		if (papercode_offset != -1 && i == papercode_offset) {
+			if (papercode_val == -1)
+				continue;
+			else if (buf_a[i] != papercode_val)
+				return INCORRECT_PAPER;
+		} else if (buf_b[i] == -1)
+			continue;
+		else if (buf_a[i] > buf_b[i])
+			return 1;
+		else if (buf_a[i] < buf_b[i])
+			return -1;
+	}
+	return 0;
+}
+
+/* Program states */
+enum {
+	S_IDLE = 0,
+	S_PRINTER_READY,
+	S_PRINTER_INIT_SENT,
+	S_PRINTER_READY_Y,
+	S_PRINTER_Y_SENT,
+	S_PRINTER_READY_M,
+	S_PRINTER_M_SENT,
+	S_PRINTER_READY_C,
+	S_PRINTER_C_SENT,
+	S_PRINTER_DONE,
+	S_FINISHED,
+};
+
+static int parse_printjob(uint8_t *buffer, int *bw_mode, int *plane_len) 
+{
+	int printer_type = -1;
+
+	if (buffer[0] != 0x40 &&
+	    buffer[1] != 0x00) {
+		goto done;
+	}
+	
+	if (buffer[12] == 0x40 &&
+	    buffer[13] == 0x01) {
+		if (buffer[2] == 0x00) {
+			printer_type = P_CP_XXX;
+			// XXX the P_CP900 is identical, but has extra
+			// data at the very end of the file.  No way to detect
+			// from a streamed source!
+		} else {
+			printer_type = P_ES1;
+			*bw_mode = (buffer[2] == 0x20);
+		}
+
+		*plane_len = *(uint32_t*)(&buffer[16]);
+		*plane_len = le32_to_cpu(*plane_len);
+		goto done;
+	}
+
+	*plane_len = *(uint32_t*)(&buffer[12]);
+	*plane_len = le32_to_cpu(*plane_len);
+
+	if (buffer[16] == 0x40 &&
+	    buffer[17] == 0x01) {
+
+		if (buffer[4] == 0x02) {
+			printer_type = P_ES2_20;
+			*bw_mode = (buffer[7] == 0x01);
+			goto done;
+		}
+    
+		if (es40_cp790_plane_lengths[buffer[2]] == *plane_len) {
+			printer_type = P_ES40_CP790; 
+			*bw_mode = (buffer[3] == 0x01);
+			goto done;
+		} else {
+			printer_type = P_ES3_30; 
+			*bw_mode = (buffer[3] == 0x01);
+			goto done;
+		}
+	}
+
+	return -1;
+
+done:
+
+	return printer_type;
+}
