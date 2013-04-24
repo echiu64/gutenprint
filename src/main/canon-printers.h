d68efe5f0fd8b9b7528b690c59ebf2a388f3d267
gernot2270 <>
2013-04-24 12:12:06 +0000

    Experimental support for Canon PIXMA MX920 family (MX922, MX923, MX924, MX925, MX926, MX927, MX928).

--- src/main/canon-printers.h
+++ src/main/canon-printers.h
@@ -2973,6 +2973,22 @@ static const canon_cap_t canon_model_capabilities[] =
     NULL,
     iP4500_channel_order
   },
+  { /* Canon MULTIPASS MX920 --- with XML CD-R tray J */
+    "PIXMA MX920", 3,          /*model, model_id*/
+    INCH(17/2), 1917,       /* max paper width and height */ /* no linux driver to check yet */
+    10, 10, 9, 15,    /*border_left, border_right, border_top, border_bottom */
+    &canon_PIXMA_MG5400_slotlist, /* iP7200 uses ESC (r 0x68 command for CD tray only */
+    CANON_CAP_STD0|CANON_CAP_DUPLEX|CANON_CAP_r|CANON_CAP_px|CANON_CAP_P|CANON_CAP_I|CANON_CAP_XML|CANON_CAP_BORDERLESS,0,
+    3,9, /* ESC (l and (P command lengths */
+    control_cmd_PIXMA_MG5300,  /* features Uses ESC (r only for CD media */
+    &canon_PIXMA_iP7200_modelist,
+    &canon_PIXMA_MG5400_paperlist,
+    &canon_PIXMA_iP7200_modeuselist,
+    NULL,
+    NULL,
+    NULL,
+    iP4500_channel_order
+  },
   { /* Canon MULTIPASS MX7600 */
     "PIXMA MX7600", 3,          /*model, model_id*/
     INCH(17/2), INCH(23),       /* max paper width and height */ /* from MacOSX driver */
