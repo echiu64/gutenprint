d7d3b55f046f3341f8f264af76ccfbf687324562
gernot2270 <>
2013-04-28 00:30:30 +0000

    Added experimental support for Canon PIXMA MP230 family and instances discovered (MP230, MP236, MP237).

--- src/main/canon-printers.h
+++ src/main/canon-printers.h
@@ -2460,6 +2460,22 @@ static const canon_cap_t canon_model_capabilities[] =
     NULL,
     iP4500_channel_order
   },
+  { /* Canon MULTIPASS MP230 --- with XML no borderless but leave it in as untested */
+    "PIXMA MP230", 3,          /*model, model_id*/
+    INCH(17/2), 1917,       /* max paper width and height */ /* from linux driver v3.80 */
+    10, 10, 9, 15,    /*border_left, border_right, border_top, border_bottom */
+    &canon_MULTIPASS_MP250_slotlist, /* these models only have a rear tray. Also uses CAP_T */
+    CANON_CAP_STD0|CANON_CAP_px|CANON_CAP_P|CANON_CAP_I|CANON_CAP_T|CANON_CAP_XML|CANON_CAP_BORDERLESS,0, /* borderless not in Windows driver---untested */
+    3,9, /* ESC (l and (P command lengths */
+    control_cmd_PIXMA_MG5300,  /*features */
+    &canon_MULTIPASS_MP230_modelist,
+    &canon_PIXMA_MG3200_paperlist,
+    &canon_MULTIPASS_MP230_modeuselist,
+    NULL,
+    NULL,
+    NULL,
+    iP4500_channel_order
+  },
   { /* Canon MULTIPASS MP240 */
     "PIXMA MP240", 3,          /*model, model_id*/
     INCH(17/2), 1917,       /* max paper width and height */ /* from MacOSX driver */
