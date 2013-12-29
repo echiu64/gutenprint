fe15a97fec7437e8023f83c51e481008da431f3a
gernot2270 <>
2013-12-29 13:52:31 +0000

    Added missing else if clause to ESC_P length condition check

--- src/main/print-canon.c
+++ src/main/print-canon.c
@@ -4357,7 +4357,7 @@ canon_init_setESC_P(const stp_vars_t *v, const canon_privdata_t *init)
       /*                             size                media                      tray */
       canon_cmd( v,ESC28,0x50,9,0x00,arg_ESCP_1,0x00,arg_ESCP_2,0x01,0x00,0x01,0x00,arg_ESCP_9);
     }
-  if ( init->caps->ESC_P_len == 8 ) /* support for new devices from 2012. */
+  else if ( init->caps->ESC_P_len == 8 ) /* support for new devices from 2012. */
     {/* the 4th of the 6 bytes is the media type. 2nd byte is media size. Both read from canon-media array. */
 
       /* arg_ESCP_1 = 0x03; */ /* A4 size */
