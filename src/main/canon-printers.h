7ccb9e49a16a032dd7ad8d503342af0e91ff78b3
gernot2270 <>
2015-05-23 15:23:26 +0000

    Added experimental support for Canon E560 series. Removed unused modes in canon-media-mode.h for E510, E610.

--- src/main/canon-printers.h
+++ src/main/canon-printers.h
@@ -3443,6 +3443,23 @@ static const canon_cap_t canon_model_capabilities[] =
     NULL,
     iP4500_channel_order
   },
+  { /* Canon MULTIPASS E560 --- with XML */
+    "PIXMA E560", 3,          /*model, model_id*/
+    INCH(17/2), 1917,       /* max paper width and height assumed */
+    10, 10, 9, 15,    /*border_left, border_right, border_top, border_bottom */
+    &canon_PIXMA_MG2100_slotlist,
+    CANON_CAP_STD0|CANON_CAP_px|CANON_CAP_P|CANON_CAP_T|CANON_CAP_I|CANON_CAP_v|CANON_CAP_XML|CANON_CAP_DUPLEX|CANON_CAP_BORDERLESS,0,
+    3,9, /* ESC (l and (P command lengths */
+    0, /* Upper/Lower Cassette option */
+    control_cmd_PIXMA_MG5300, /*features*/
+    &canon_MULTIPASS_E560_modelist,
+    &canon_PIXMA_iX6800_paperlist,
+    &canon_MULTIPASS_E560_modeuselist,
+    NULL,
+    NULL,
+    NULL,
+    iP4500_channel_order
+  },
   { /* Canon MULTIPASS E600 --- with XML */
     "PIXMA E600", 3,          /*model, model_id*/
     INCH(17/2), 1917,       /* max paper width and height */ /* from linux driver v3.70 */
