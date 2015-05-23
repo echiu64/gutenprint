ca7984a4183862e9ca1e263c8df3c7a483e18391
gernot2270 <>
2015-05-23 16:39:03 +0000

    Added experimental support for Canon E400 series.

--- src/main/canon-printers.h
+++ src/main/canon-printers.h
@@ -3409,6 +3409,23 @@ static const canon_cap_t canon_model_capabilities[] =
     NULL,
     iP4500_channel_order
   },
+  { /* Canon MULTIPASS E400 --- with XML */
+    "PIXMA E400", 3,          /*model, model_id*/
+    INCH(17/2), 1917,       /* max paper width and height assumed */
+    10, 10, 9, 15,    /*border_left, border_right, border_top, border_bottom */
+    &canon_MULTIPASS_MP250_slotlist,
+    CANON_CAP_STD0|CANON_CAP_px|CANON_CAP_P|CANON_CAP_T|CANON_CAP_I|CANON_CAP_v|CANON_CAP_XML,0,
+    3,9, /* ESC (l and (P command lengths */
+    0, /* Upper/Lower Cassette option */
+    control_cmd_PIXMA_MG5300,
+    &canon_PIXMA_P200_modelist,
+    &canon_PIXMA_P200_paperlist,
+    &canon_PIXMA_P200_modeuselist,
+    NULL,
+    NULL,
+    NULL,
+    iP4500_channel_order
+  },
   { /* Canon MULTIPASS E500 --- with XML */
     "PIXMA E500", 3,          /*model, model_id*/
     INCH(17/2), 1917,       /* max paper width and height */ /* from linux driver v3.60 */
