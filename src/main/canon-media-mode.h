6c3afc64696343e4590fa722a2293830f95ac493
Gernot Hassenpflug <gernot2270@users.sourceforge.net>
2017-07-09 00:36:59 +0900

    Added experimental support for Canon PIXMA TS5000.

--- src/main/canon-media-mode.h
+++ src/main/canon-media-mode.h
@@ -10797,6 +10797,71 @@ static const canon_modeuse_t canon_PIXMA_E3100_modeuses[] = {
 
 DECLARE_MODEUSES(canon_PIXMA_E3100);
 
+/* ----------------------------------- Canon TS5000  ----------------------------------- */
+static const char* canon_PIXMA_TS5000_modeuses_plain[] = {
+  "600x600dpi_high",
+  "600x600dpi",
+  "600x600dpi_draft",
+  NULL
+  };
+
+static const char* canon_PIXMA_TS5000_modeuses_PPplusG2[] = {
+  "600x600dpi_photohigh",
+  "600x600dpi_photo",
+  NULL
+};
+
+static const char* canon_PIXMA_TS5000_modeuses_PPpro[] = {
+  /*ud1 not supported */
+  "600x600dpi_photohigh",
+  "600x600dpi_photo",
+  NULL
+};
+
+static const char* canon_PIXMA_TS5000_modeuses_Hagaki[] = {
+  "600x600dpi_high3",
+  "600x600dpi_std3",
+  NULL
+};
+
+static const char* canon_PIXMA_TS5000_modeuses_inkjetHagaki[] = {
+  "600x600dpi_photohigh2",
+  "600x600dpi_photo",
+  NULL
+};
+
+static const char* canon_PIXMA_TS5000_modeuses_TShirt[] = {
+  "600x600dpi_tshirt",
+  NULL
+};
+
+static const char* canon_PIXMA_TS5000_modeuses_PPother[] = {
+  "600x600dpi_photohigh",
+  NULL
+};
+
+static const canon_modeuse_t canon_PIXMA_TS5000_modeuses[] = {
+  { "Plain",            canon_PIXMA_TS5000_modeuses_plain, 0 },
+  { "PhotoPlusGloss2",  canon_PIXMA_TS5000_modeuses_PPplusG2, 0 },
+  { "PhotoProPlat",     canon_PIXMA_TS5000_modeuses_PPpro, 0 },
+  { "PhotoProLuster",   canon_PIXMA_TS5000_modeuses_PPplusG2, 0 },
+  { "PhotoProSemiGloss",canon_PIXMA_TS5000_modeuses_PPplusG2, 0 },
+  { "PhotopaperMatte",	canon_PIXMA_TS5000_modeuses_PPplusG2, 0 },
+  { "GlossyPaperStandard",canon_PIXMA_TS5000_modeuses_PPplusG2, 0 },
+  { "Coated",		canon_PIXMA_TS5000_modeuses_PPplusG2, 0 },
+  { "InkJetHagakiaddr", canon_PIXMA_TS5000_modeuses_Hagaki, 0 },
+  { "InkJetHagaki", 	canon_PIXMA_TS5000_modeuses_inkjetHagaki, 0 },
+  { "InkjetPhotoHagakiKaddr",canon_PIXMA_TS5000_modeuses_Hagaki, 0 },
+  { "InkjetPhotoHagakiK",canon_PIXMA_TS5000_modeuses_PPplusG2, 0 },
+  { "HagakiA", 	        canon_PIXMA_TS5000_modeuses_Hagaki, 0 },
+  { "Hagaki", 	        canon_PIXMA_TS5000_modeuses_Hagaki, 0 },
+  { "TShirt",		canon_PIXMA_TS5000_modeuses_TShirt, 0 },
+  { "Envelope",		canon_PIXMA_TS5000_modeuses_Hagaki, 0 },
+  { "PhotopaperOther",	canon_PIXMA_TS5000_modeuses_PPother, 0 },
+};
+
+DECLARE_MODEUSES(canon_PIXMA_TS5000);
+
 /* ----------------------------------- Canon G1000  ----------------------------------- */
 static const char* canon_PIXMA_G1000_modeuses_plain[] = {
   "600x600dpi_high",
--- src/main/canon-printers.h
+++ src/main/canon-printers.h
@@ -3783,6 +3783,23 @@ static const canon_cap_t canon_model_capabilities[] =
     NULL,
     iP4500_channel_order
   },
+  { /* Canon PIXMA TS5000 --- with XML, borderless */
+    "PIXMA TS5000", 3,          /*model, model_id*/
+    INCH(17/2), 1917,       /* max paper width and height */
+    10, 10, 9, 15,    /*border_left, border_right, border_top, border_bottom */
+    &canon_MULTIPASS_MP250_slotlist, /* these models only have a rear tray. */
+    CANON_CAP_STD0|CANON_CAP_px|CANON_CAP_P|CANON_CAP_I|CANON_CAP_v|CANON_CAP_w|CANON_CAP_XML|CANON_CAP_BORDERLESS,0,
+    3,9, /* ESC (l and (P command lengths */
+    0, /* Upper/Lower Cassette option */
+    control_cmd_PIXMA_MG5600,  /*features */
+    &canon_PIXMA_TS5000_modelist,
+    &canon_PIXMA_TS5000_paperlist,
+    &canon_PIXMA_TS5000_modeuselist,
+    NULL,
+    NULL,
+    NULL,
+    iP4500_channel_order
+  },
   { /* Canon PIXMA G1000 --- with XML */
     "PIXMA G1000", 3,          /*model, model_id*/
     INCH(17/2), 1917,       /* max paper width and height assumed */
