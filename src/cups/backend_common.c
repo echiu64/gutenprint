5ee6fcef88290c334b23610d59bd5f7533a67a8f
Solomon Peachy <pizza@shaftnet.org>
2020-01-16 08:07:45 -0500

    dyesub: backend:  Fix a bug in the BCD-to-Integer conversion code.
    (Fixes the lifetime print counter reporting on the Mitsu D70 family)

--- src/cups/backend_common.c
+++ src/cups/backend_common.c
@@ -30,7 +30,7 @@
 #include <errno.h>
 #include <signal.h>
 
-#define BACKEND_VERSION "0.98G"
+#define BACKEND_VERSION "0.98.1G"
 #ifndef URI_PREFIX
 #error "Must Define URI_PREFIX"
 #endif
@@ -1524,7 +1524,7 @@ uint32_t packed_bcd_to_uint32(const char *in, int len)
 
 	while (len--) {
 		out *= 10;
-		out += (*in >> 4);
+		out += ((*in >> 4) & 0xf);
 		out *= 10;
 		out += (*in & 0xf);
 		in++;
